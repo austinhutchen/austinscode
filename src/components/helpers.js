20d824c22cad6e820ba2e5a2f2e775c0a558b4d9
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-09 06:52:39 -0600

    d

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -22,7 +22,9 @@ export const Route = () => {
             class="btn btn-md btn-outline-secondary"
             type="button"
             id="navbar"
-            onClick={()=>{nav("../views/resume.html")}}
+            onClick={() => {
+              nav("../views/resume.html");
+            }}
           >
             Resume
           </button>
@@ -30,7 +32,9 @@ export const Route = () => {
             class="btn btn-md btn-outline-secondary"
             type="button"
             id="navbar"
-            onClick={()=>{nav("../views/projects.html")}}
+            onClick={() => {
+              nav("../views/projects.html");
+            }}
           >
             Projects
           </button>
@@ -38,7 +42,9 @@ export const Route = () => {
             class="btn btn-md btn-outline-secondary"
             type="button"
             id="navbar"
-            onClick={()=>{nav("../views/about.html")}}
+            onClick={() => {
+              nav("../views/about.html");
+            }}
           >
             Hobbies
           </button>
@@ -63,7 +69,7 @@ export const Head = () => {
         <h1 id="name">Austin Hutchen</h1>
       </div>
       <header>
-        <i id="desc">
+        <i id="desc" style={{marginTop:-20}}>
           Prospective software engineer with passion for development and
           experience working with startups!
         </i>
@@ -83,34 +89,28 @@ export const Body = () => {
           <i> Relevant Work Experience</i>
         </h1>
         <hr className="break" />
-            <b>
-              <u>
-                Mobius Materials, Marketing Consultant (Summer 2020-Fall 2020)
-              </u>
-            </b>
-            <article>
-              <p>
-                {" "}
-                Made first company sale as Marketing consultant, worked closely
-                with the software engineering team to achieve forecasted
-                results.{" "}
-              </p>
-            </article>
+        <b>
+          <u>Mobius Materials, Marketing Consultant (Summer 2020-Fall 2020)</u>
+        </b>
+        <article>
+          <p>
+            {" "}
+            Made first company sale as Marketing consultant, worked closely with
+            the software engineering team to achieve forecasted results.{" "}
+          </p>
+        </article>
 
-            <b>
-              <u>
-                Calculus 2 Course Assistant (University Of Colorado Boulder)
-              </u>
-            </b>
-            <article id="font">
-              Taught CACLULUS 2 alongside faculty at CU boulder. Also held
-              office hours every week to assist learning of course material.{" "}
-              <b>
-                Recieved 97% positive feedback from 103 participants on end of
-                semester survey.
-              </b>
-            </article>
-
+        <b>
+          <u>Calculus 2 Course Assistant (University Of Colorado Boulder)</u>
+        </b>
+        <article id="font">
+          Taught CACLULUS 2 alongside faculty at CU boulder. Also held office
+          hours every week to assist learning of course material.{" "}
+          <b>
+            Recieved 97% positive feedback from 103 participants on end of
+            semester survey.
+          </b>
+        </article>
       </body>
       <hr className="break" />
     </>
@@ -164,7 +164,7 @@ export const Foot = () => {
         Advised twitter on optimizations for their recommendation algorithms,
         most notably the use of floating point vectorisations.
       </p>
-   
+
       <h2 className="hlight">
         <b>ACCREDATIONS:</b>
       </h2>
@@ -173,7 +173,6 @@ export const Foot = () => {
         <b>Calculus 2 Instructor Letter Of Recommendation</b>
       </a>
 
-     
       <br></br>
     </footer>
   );
---
+++ src/views/about.js
@@ -0,0 +1,15 @@
+
+
+ export const func=()=>{
+
+  return(
+<html>
+<h1>
+ hello world
+</h1>
+
+
+</html>
+
+  )
+ }
