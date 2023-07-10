d600200c2245980a3c7121cf84385fec8d897b09
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-10 07:26:46 -0600

    d

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Head, Body, Foot, Nav } from "./components/helpers";
 import { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Landing} from "./components/landing"
+import {Resume} from "./components/resume"
 // main app
 
 class MainApp extends Component {
@@ -9,10 +9,12 @@ class MainApp extends Component {
     super(props);
     this.render.bind(this);
     this.state = {
+      // state for app is modified here
       today: new Date(),
       date: "",
     };
   }
+
   setData() {
     this.setState({
       date:
@@ -23,6 +25,64 @@ class MainApp extends Component {
         this.state.today.getFullYear().toString(),
     });
   }
+    Nav = () => {
+    const nav = useNavigate();
+    return (
+      <div>
+        <nav
+          class="navbar navbar-dark bg-dark fixed-top"
+          href="../views/hobbies.html"
+        >
+          <div>
+            <button
+              class="btn btn-outline-success me-2"
+              type="button"
+              id="navbar"
+            >
+              Home
+            </button>
+            <button
+              class="btn btn-md btn-outline-secondary"
+              type="button"
+              id="navbar"
+              onClick={() => {
+                nav("./resume.js");
+              }}
+            >
+              Resume
+            </button>
+            <button
+              class="btn btn-md btn-outline-secondary"
+              type="button"
+              id="navbar"
+              onClick={() => {
+                nav("/");
+              }}
+            >
+              Projects
+            </button>
+            <button
+              class="btn btn-md btn-outline-secondary"
+              type="button"
+              id="navbar"
+              onClick={() => {
+                nav("/");            }}
+            >
+              Hobbies
+            </button>
+          </div>
+          <a class="navbar-brand">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/5024/5024509.png"
+              alt="icon"
+              width="30"
+              height="30"
+            />
+          </a>
+        </nav>
+      </div>
+    );
+  };
   // javascript goes here
   render() {
     this.setData();
@@ -31,6 +91,7 @@ class MainApp extends Component {
       <BrowserRouter>
       <Routes>
       <Route exact path="/" element={<Landing/>}/>
+      <Route exact path="./components/resume.js" element={<Resume/>}/>
       </Routes>
       </BrowserRouter>
       </>
---
+++ src/components/resume.js
@@ -0,0 +1,14 @@
+
+
+
+
+export const Resume =()=>{
+return(
+ <h1>
+  Hello world this is my resume page
+ </h1>
+)
+
+
+
+}
