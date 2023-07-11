734b0397081cead8fdaf59b1453a882ecc038ecb
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-11 06:46:33 -0700

    rm unused components

--- src/App.js
+++ src/App.js
@@ -26,64 +26,7 @@ class MainApp extends Component {
         this.state.today.getFullYear().toString(),
     });
   }
-    Nav = () => {
-    const nav = useNavigate();
-    return (
-      <div>
-        <nav
-          class="navbar navbar-dark bg-dark fixed-top"
-          href="../views/hobbies.html"
-        >
-          <div>
-            <button
-              class="btn btn-outline-success me-2"
-              type="button"
-              id="navbar"
-            >
-              Home
-            </button>
-            <button
-              class="btn btn-md btn-outline-secondary"
-              type="button"
-              id="navbar"
-              onClick={() => {
-                nav("./resume.js");
-              }}
-            >
-              Resume
-            </button>
-            <button
-              class="btn btn-md btn-outline-secondary"
-              type="button"
-              id="navbar"
-              onClick={() => {
-                nav("/");
-              }}
-            >
-              Projects
-            </button>
-            <button
-              class="btn btn-md btn-outline-secondary"
-              type="button"
-              id="navbar"
-              onClick={() => {
-                nav("/");            }}
-            >
-              Hobbies
-            </button>
-          </div>
-          <a class="navbar-brand">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/5024/5024509.png"
-              alt="icon"
-              width="30"
-              height="30"
-            />
-          </a>
-        </nav>
-      </div>
-    );
-  };
+
   // javascript goes here
   render() {
     this.setData();
