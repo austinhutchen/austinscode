bebfdeb9c523d7dfd6c0babdbdc42ccae23263ec
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-14 07:56:31 -0700

    JS setup for animations

--- src/animate/observer.js
+++ src/animate/observer.js
@@ -1,16 +1,23 @@
+import { useEffect } from "react";
 
 
-
-
-const observer = new IntersectionObserver((entries)=>{
- entries.forEach((entry)=>{
-  console.log(entry)
-  if(entry.IsIntersecting){
-   entry.target.classlist.add('show');
-  }
+useEffect(()=>{
+ const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+   console.log(entry)
+   if(entry.isIntersecting){
+    entry.target.classList.add('show');
+   }
+   else{
+    entry.target.classList.remove('show');
+ 
+   }
+  });
  });
-});
+ 
+ const hidden = new document.querySelectorAll('.hidden');
+ hidden.forEach((el)=>observer.observe(el));
+})
 
-const hidden = new document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=>observer.observe(el));
+
 
