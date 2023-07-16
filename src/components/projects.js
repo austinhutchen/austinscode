07cb03ad515f510c914d83d8b9ff711095cea861
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-16 12:13:17 -0700

    css spiner

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,55 +11,69 @@ export const Projects = () => {
             </h2>
 
             <section>
-                <b >
-                    <a href="https://github.com/austinhutchen/encrypt.C.git" target="_blank" rel="noreferrer">
-                        FILE TEXT I/O PARSER AND COMPRESSION ALGORITHM (C++)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/IOS-calculator-app" target="_blank" rel="noreferrer">
-                        Functional calculator App (FLUTTER/DART)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/portfolio-rewritten.git" target="_blank" rel="noreferrer">
-                        This Webpage (REACT/JS)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/weather-native.git" target="_blank" rel="noreferrer">
-                        Weather app (REACT NATIVE)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/redditsearch" target="_blank" rel="noreferrer">
-                        Reddit search/statistics engine (PYTHON)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/shell.git" target="_blank" rel="noreferrer">
-                        Kernel Shell (VANILLA C)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/ArtificalSP.git" target="_blank" rel="noreferrer">
-                        Intelligent Audio Analyzer (C+Tensorflow)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/cmalloc.git" target="_blank" rel="noreferrer">
-                        Fully functional malloc() implementation
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/2d-walk.git "target="_blank" rel="noreferrer">
-                        Portable Breadboard gaming computer (C)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/notable.git" target="_blank" rel="noreferrer">
-                        Journalling app (REACT NATIVE/JAVA)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/GAMMA.JS.git" target="_blank" rel="noreferrer">
-                        Graphing calculator in 2-3 dimensions (JAVASCRIPT)
-                    </a>
-                    <br></br>
-                    <a href="https://github.com/austinhutchen/map-app.git" target="_blank" rel="noreferrer">
-                        Map APP (Flutter/C++)
-                    </a>
-                </b>
+                <div className="spinner">
+            
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+
+                <div className="lead">
+                    <b >
+                        <a href="https://github.com/austinhutchen/encrypt.C.git" target="_blank" rel="noreferrer">
+                            FILE TEXT I/O PARSER AND COMPRESSION ALGORITHM (C++)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/IOS-calculator-app" target="_blank" rel="noreferrer">
+                            Functional calculator App (FLUTTER/DART)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/portfolio-rewritten.git" target="_blank" rel="noreferrer">
+                            This Webpage (REACT/JS)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/weather-native.git" target="_blank" rel="noreferrer">
+                            Weather app (REACT NATIVE)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/redditsearch" target="_blank" rel="noreferrer">
+                            Reddit search/statistics engine (PYTHON)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/shell.git" target="_blank" rel="noreferrer">
+                            Kernel Shell (VANILLA C)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/ArtificalSP.git" target="_blank" rel="noreferrer">
+                            Intelligent Audio Analyzer (C+Tensorflow)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/cmalloc.git" target="_blank" rel="noreferrer">
+                            Fully functional malloc() implementation
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/2d-walk.git " target="_blank" rel="noreferrer">
+                            Portable Breadboard gaming computer (C)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/notable.git" target="_blank" rel="noreferrer">
+                            Journalling app (REACT NATIVE/JAVA)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/GAMMA.JS.git" target="_blank" rel="noreferrer">
+                            Graphing calculator in 2-3 dimensions (JAVASCRIPT)
+                        </a>
+                        <br></br>
+                        <a href="https://github.com/austinhutchen/map-app.git" target="_blank" rel="noreferrer">
+                            Map APP (Flutter/C++)
+                        </a>
+                    </b>
+                </div>
+
                 <br></br>
                 <h2 className="hlight">
                     <b>OPEN SOURCE:</b>
