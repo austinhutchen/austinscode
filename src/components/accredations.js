a544ee10add23d90ef4501082c6e80ebc01952e4
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-21 11:47:10 -0700

    done

--- src/components/accredations.js
+++ src/components/accredations.js
@@ -8,8 +8,8 @@ export const PDFViewer = () => {
   <div className="App" >
    <Nav></Nav>
    <h1><b><i><u>Letter of Accredation from Math PHD @ CU Boulder</u></i></b></h1>
-<br></br>
-   <embed src={pdf} height="600" width="500" style={{ paddingTop: 30 }} />
+   <br></br>
+   <embed src={pdf} height="600" width="500" style={{ paddingTop: 15 }} />
   </div>
  )
 
