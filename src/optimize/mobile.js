9cae0f21bdec25f57ff41842aad12d2def555d69
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-21 11:49:19 -0700

    d

---
+++ src/optimize/mobile.js
@@ -0,0 +1,12 @@
+
+// mobile wrapper 
+export const MyWrapperComponent = (props) => {
+ const isMobile = useMediaQuery({ query: '(max-width: 1000px)' });
+ const textStyle = isMobile ? 'text-mobile' : 'text-desktop';
+
+ return (
+   <div className={textStyle}>
+    {props.children}
+   </div>
+ )
+}
