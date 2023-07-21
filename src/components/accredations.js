4e13beadfc23679894b3d8e4bd37b4e127b4ade7
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-21 12:00:11 -0700

    d

--- src/components/accredations.js
+++ src/components/accredations.js
@@ -5,7 +5,7 @@ import pdf from "./images/Recommendation Letter_ Austin.pdf"
 
 export const PDFViewer = () => {
  return (
-  <div className="App" >
+  <div className="App"  >
    <Nav></Nav>
    <h1><b><i><u>Letter of Accredation from Math PHD @ CU Boulder</u></i></b></h1>
    <br></br>
