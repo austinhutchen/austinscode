28b4828b7c913fef3eeb04259acbd24f51600d50
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-22 09:19:47 -0700

    animated routes

--- src/components/AnimatedRoutes.js
+++ src/components/AnimatedRoutes.js
@@ -1,25 +1,30 @@
 
-
 import { Routes, Route, useLocation } from "react-router-dom";
+import { Landing } from "./components/landing"
+import { Resume } from "./components/resume"
+import { Projects } from "./components/projects"
+import { PDFViewer } from "./components/accredations"
+import { Hobbies } from "./components/hobbies"
+import { HashRouter } from "react-router-dom";
 
 
 
-export let AnimatedRoutes=()=>{
- const location = useLocation();
-return(
- <div className="App">
-    <HashRouter >
-          <Routes>
-            <Route exact path="" element={<Landing />} />
-            <Route exact path="src/components/landing.js" element={<Landing />} />
-            <Route exact path="src/components/resume.js" element={<Resume />} />
-            <Route exact path="src/components/projects.js" element={<Projects />} />
-            <Route exact path="src/components/hobbies.js" element={<Hobbies />} />
-            <Route exact path="src/components/accredations.js" element={<Reader />} />
-          </Routes>
-        </HashRouter>
+export let AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <div className="App">
+      <HashRouter >
+        <Routes location={location} key={location.pathname}>
+          <Route exact path="" element={<Landing />} />
+          <Route exact path="src/components/landing.js" element={<Landing />} />
+          <Route exact path="src/components/resume.js" element={<Resume />} />
+          <Route exact path="src/components/projects.js" element={<Projects />} />
+          <Route exact path="src/components/hobbies.js" element={<Hobbies />} />
+          <Route exact path="src/components/accredations.js" element={<PDFViewer />} />
+        </Routes>
+      </HashRouter>
 
- </div>
-)
+    </div>
+  )
 
 }
