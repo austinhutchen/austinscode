c7ac32c025d08edcfb1cf2ee4d50f6c1c93feac3
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-22 09:41:12 -0700

    basic page animations

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,39 +4,43 @@ import { motion } from "framer-motion";
 
 export const Resume = () => {
   return (
-    <motion.div className="App" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-
+    <section>
       <Nav />
-      <body id="desc" >
-        <h1 className="hlight" >
-          <i> Relevant Work Experience</i>
-        </h1>
-        <hr className="break" />
-        <b>
-          <u>Mobius Materials, Marketing Consultant (Summer 2020-Fall 2020)</u>
-        </b>
-        <article>
-          <p>
-            {" "}
-            Made first company sale as Marketing consultant, worked closely with
-            the software engineering team to achieve forecasted results.{" "}
-          </p>
-        </article>
+      <motion.div className="App" initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth }}>
 
-        <b>
-          <u>Calculus 2 Course Assistant (University Of Colorado Boulder)</u>
-        </b>
-        <article>
-          Taught CACLULUS 2 alongside faculty at CU boulder. Also held office
-          hours every week to assist learning of course material.{" "}
+
+        <body id="desc" >
+          <h1 className="hlight" >
+            <i> Relevant Work Experience</i>
+          </h1>
+          <hr className="break" />
           <b>
-            Recieved 97% positive feedback from 103 participants on end of
-            semester survey.
+            <u>Mobius Materials, Marketing Consultant (Summer 2020-Fall 2020)</u>
           </b>
-        </article>
-      </body>
-      <hr className="break" />
-    </motion.div>
+          <article>
+            <p>
+              {" "}
+              Made first company sale as Marketing consultant, worked closely with
+              the software engineering team to achieve forecasted results.{" "}
+            </p>
+          </article>
+
+          <b>
+            <u>Calculus 2 Course Assistant (University Of Colorado Boulder)</u>
+          </b>
+          <article>
+            Taught CACLULUS 2 alongside faculty at CU boulder. Also held office
+            hours every week to assist learning of course material.{" "}
+            <b>
+              Recieved 97% positive feedback from 103 participants on end of
+              semester survey.
+            </b>
+          </article>
+        </body>
+        <hr className="break" />
+      </motion.div>
+
+    </section>
 
   )
 
