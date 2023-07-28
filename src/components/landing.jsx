31426d9cf5acd6e6d94fdbfaae7a84ceaa738808
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-28 09:17:08 -0700

    d

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 import "../css/spinner.css"
 import "../css/spotify.css";
 import "../css/nav-bar.css";
-
+import "../css/search.css"
 export const Nav = () => {
     const nav = useNavigate();
     return (
@@ -162,7 +162,7 @@ const Player = () => {
     // uses spotify.css to render
     return (
         <div class="card" className="App">
-          
+
 
             <div class="controls">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height="20" width="24"
@@ -202,7 +202,7 @@ const Player = () => {
             <div class="time">
                 <div class="elapsed"></div>
             </div>
-       
+
             <p class="timetext time_now">1:31</p>
             <p class="timetext time_full">3:46</p>
         </div>
@@ -226,12 +226,28 @@ const Spinner = () => {
     )
 }
 
+const Search =()=>{
+return(
+    <div class="container">
+                <div class="search-container">
+                    <input class="input" type="text" />
+                    <svg viewBox="0 0 24 24" class="search__icon">
+                        <g>
+                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                            </path>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+)
+}
+
 export const Landing = () => {
     // add Player and Spinner, get css animations working
     return (
-        <section className="App"> 
+        <section className="App">
             <Head />
-            <motion.div  initial={{ width: 0 }} animate={{ width: "100%" }}
+            <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
                 exit={{ x: window.innerWidth, transition: { duration: 0 } }}>
                 <i
                     id="desc"
@@ -245,6 +261,7 @@ export const Landing = () => {
             </motion.div>
             <Player />
             <Spinner />
+           
         </section>
 
 
