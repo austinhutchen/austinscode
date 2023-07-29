607dea845f42fdcba251f385bdfeedd839e5f21b
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-29 08:42:38 -0700

    d

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -29,6 +29,16 @@ export const Nav = () => {
                         type="button"
                         id="navbar"
                         onClick={() => {
+                            nav('/src/components/aboutme.js');
+                        }}
+                    >
+                        About
+                    </button>
+                    <button
+                        class="btn btn-md btn-outline-secondary"
+                        type="button"
+                        id="navbar"
+                        onClick={() => {
                             nav('/src/components/resume.js');
                         }}
                     >
@@ -43,16 +53,6 @@ export const Nav = () => {
                         }}
                     >
                         Projects
-                    </button>
-                    <button
-                        class="btn btn-md btn-outline-secondary"
-                        type="button"
-                        id="navbar"
-                        onClick={() => {
-                            nav('/src/components/aboutme.js');
-                        }}
-                    >
-                        AboutMe
                     </button>
                     <button
                         class="btn btn-md btn-outline-secondary"
@@ -226,20 +226,20 @@ const Spinner = () => {
     )
 }
 
-const Search =()=>{
-return(
-    <div class="container">
-                <div class="search-container">
-                    <input class="input" type="text" />
-                    <svg viewBox="0 0 24 24" class="search__icon">
-                        <g>
-                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
-                            </path>
-                        </g>
-                    </svg>
-                </div>
+const Search = () => {
+    return (
+        <div class="container">
+            <div class="search-container">
+                <input class="input" type="text" />
+                <svg viewBox="0 0 24 24" class="search__icon">
+                    <g>
+                        <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                        </path>
+                    </g>
+                </svg>
             </div>
-)
+        </div>
+    )
 }
 
 export const Landing = () => {
@@ -261,7 +261,7 @@ export const Landing = () => {
             </motion.div>
             <Player />
             <Spinner />
-           
+
         </section>
 
 
