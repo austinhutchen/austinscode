f80d4600d346d4b06bf89e08b007a8adc75c2569
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-01 00:26:45 -0700

    d

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,5 +1,38 @@
 import { motion } from 'framer-motion'
 import { Nav } from "./landing";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+
+const Display = () => {
+    // use carousel slide to display hobbies with images
+    return (
+        <section className="App">
+            <h1 className="hlight" >
+                More about me!
+            </h1>
+            <Carousel>
+
+                <div>
+                    <img src={img} height="520" width="auto" style={{ marginTop: 20 }} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+        </section>
+
+
+    )
+}
+
+
 
 export const Projects = () => {
 
