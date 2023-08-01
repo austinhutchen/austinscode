b15bbfb372f2a37bafdb970e0bc42fdc2b34a39f
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-01 01:14:58 -0700

    d

--- src/components/accredations.jsx
+++ src/components/accredations.jsx
@@ -4,11 +4,10 @@ import {motion} from 'framer-motion'
 // reusable with props
 export const PDFViewer = (props) => {
     return (
-
         <section>
             <motion.div className="App" initial={{width: 0}} animate={{width: "100%"}}
                         exit={{x: window.innerWidth, transition: {duration: 0}}}>
-                <br></br>
+                <br/>
                 <embed src={props.data} height="600" width="500" className="img"/>
             </motion.div>
         </section>
