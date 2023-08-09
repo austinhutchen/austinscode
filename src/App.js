62ac1d9416f2cc5ba2f066bc3e1f656f06786b76
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-08 18:04:40 -0700

    performance improved

--- src/App.js
+++ src/App.js
@@ -5,33 +5,12 @@ import { HashRouter } from "react-router-dom";
 // main app
 
 class MainApp extends Component {
-  constructor(props) {
-    super(props);
-    this.render.bind(this);
-    this.state = {
-      // state for app is modified here
-      today: new Date(),
-      date: "",
-    };
-  }
-
-  setData() {
-    this.setState({
-      date:
-        this.state.today.getMonth().toString() +
-        "/" +
-        this.state.today.getDate().toString() +
-        "/" +
-        this.state.today.getFullYear().toString(),
-    });
-  }
 
   // javascript goes here
   render() {
-    this.setData();
     return (
       <HashRouter>
-        <AnimatedRoutes/>
+        <AnimatedRoutes />
       </HashRouter>
     );
   }
