70be4e6a9677fb48d47c187390e53946e6fc6746
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-11 08:09:43 -0700

    optimize for larger screens

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -7,7 +7,7 @@ import coding from "./images/coding.JPG"
 import { motion } from 'framer-motion'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import { ReactFragment } from "react"
+import useWindowDimensions from'./optimize'
 export const Aboutme = () => {
     // optimize motion div with for desktop and mobile HERE
     return (
@@ -24,8 +24,7 @@ export const Aboutme = () => {
 export const Display = (props) => {
     // use carousel slide to display hobbies with images
     const arr = [props.img1, props.img2, props.img3, props.img4, props.img5, props.img6, props.img7, props.img8]
-    const height = 700;
-    const width = 500;
+    const { height, width } = useWindowDimensions();
     return (
         <div >
             <section style={{ paddingBottom: 20 }}>
@@ -33,26 +32,27 @@ export const Display = (props) => {
                     More about me!
                 </h1>
             </section>
-            <Carousel>
+            <Carousel sliderWidth={width/2}
+                itemWidth={width/2}>
                 <div>
-                    <img src={arr[0]} height={height} width={width} />
+                    <img src={arr[0]} height={height} width="auto" />
                     <p className="legend">  My name is <i><b></b>Austin Hutchen</i>. I'm a software engineer with aspirations of working on embedded systems and machine learning, I have plenty of hobbies! Swipe to learn more:
                     </p>
                 </div>
                 <div>
-                    <img src={arr[1]} height={height} width={width} />
+                    <img src={arr[1]} height={height} width="auto" />
                     <p className="legend">I love cats!</p>
                 </div>
                 <div>
-                    <img src={arr[2]} height={height} width={width} />
+                    <img src={arr[2]} height={height} width="auto" />
                     <p className="legend">I love quality family time!</p>
                 </div>
                 <div>
-                    <img src={arr[3]} height={height} width={width} />
+                    <img src={arr[3]} height={height} width="auto" />
                     <p className="legend">I love hiking!</p>
                 </div>
                 <div>
-                    <img src={arr[4]} height={height} width={width} />
+                    <img src={arr[4]} height={height} width="auto" />
                     <p className="legend">I love coffee and coding!</p>
                 </div>
 
@@ -60,3 +60,4 @@ export const Display = (props) => {
         </div>
     )
 }
+
