12a299ffd6b23ad737e7ab5456a5bfc75e4ed6d0
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-18 09:27:46 -0700

    DELETE OPTIMIZE>JS

--- src/components/base/optimize.jsx
+++ src/components/base/optimize.jsx
@@ -22,3 +22,15 @@ export default function useWindowDimensions() {
 
   return windowDimensions;
 }
+
+// mobile wrapper 
+export const MyWrapperComponent = (props) => {
+  const isMobile = useMediaQuery({query: '(max-width: 1000px)'});
+  const textStyle = isMobile ? 'text-mobile' : 'text-desktop';
+
+  return (
+      <div className={textStyle}>
+          {props.children}
+      </div>
+  )
+}
