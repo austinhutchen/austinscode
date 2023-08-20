583210e4486ed835444a2d1956dbda01662afcb5
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-20 08:38:17 -0700

    footer sticky

--- src/components/pages/resume.jsx
+++ src/components/pages/resume.jsx
@@ -1,56 +1,57 @@
-import {Nav} from "../common/navbar";
-import {motion} from "framer-motion";
-import {PDFViewer} from "../common/PDFviewer";
+import { Nav } from "../common/navbar";
+import { motion } from "framer-motion";
+import { PDFViewer } from "../common/PDFviewer";
 import pdf from "../images/resume SWE.pdf"
+import { Footer } from "../common/footer";
 
 export const Resume = () => {
     return (
         <section>
-            <Nav/>
-            <motion.div className="App" initial={{width: 0}} animate={{width: "100%"}}
-                        exit={{x: window.innerWidth, transition: {duration: 0}}}>
+            <Nav />
+            <motion.div className="App" initial={{ width: 0 }} animate={{ width: "100%" }}
+                exit={{ x: window.innerWidth, transition: { duration: 0 } }}>
 
 
                 <body id="desc">
-                <h1 className="hlight">
-                    <b><i> DIRECT WORK EXPERIENCE</i></b>
-                </h1>
-                <hr className="break"/>
-                <b>
-                    <u>Mobius Materials, Marketing Consultant (Summer 2020-Fall 2020)</u>
-                </b>
-                <article>
-                    <p>
-                        {" "}
-                        Made first company sale as Marketing consultant, worked closely with
-                        the software engineering team to achieve forecasted results.{" "}
-                    </p>
-                </article>
-
-                <b>
-                    <u>Calculus 2 Course Assistant (University Of Colorado Boulder)</u>
-                </b>
-                <article>
-                    Taught CACLULUS 2 alongside faculty at CU boulder. Also held office
-                    hours every week to assist learning of course material.{" "}
+                    <h1 className="hlight">
+                        <b><i> DIRECT WORK EXPERIENCE</i></b>
+                    </h1>
+                    <hr className="break" />
                     <b>
-                        Recieved 97% positive feedback from 103 participants on end of
-                        semester survey.
+                        <u>Mobius Materials, Marketing Consultant (Summer 2020-Fall 2020)</u>
                     </b>
-                </article>
+                    <article>
+                        <p>
+                            {" "}
+                            Made first company sale as Marketing consultant, worked closely with
+                            the software engineering team to achieve forecasted results.{" "}
+                        </p>
+                    </article>
+
+                    <b>
+                        <u>Calculus 2 Course Assistant (University Of Colorado Boulder)</u>
+                    </b>
+                    <article>
+                        Taught CACLULUS 2 alongside faculty at CU boulder. Also held office
+                        hours every week to assist learning of course material.{" "}
+                        <b>
+                            Recieved 97% positive feedback from 103 participants on end of
+                            semester survey.
+                        </b>
+                    </article>
                 </body>
-                <br/>
+                <br />
                 <h1 className="hlight">
                     <b><i> OPEN SOURCE CONTRIBUTIONS</i></b>
                 </h1>
-                <hr className="break"/>
+                <hr className="break" />
 
 
-                <a href="https://github.com/twitter/the-algorithm/issues/162" style={{margin: 25}} target="_blank"
-                   rel="noreferrer">
+                <a href="https://github.com/twitter/the-algorithm/issues/162" style={{ margin: 25 }} target="_blank"
+                    rel="noreferrer">
                     <b>TWITTER</b>
                 </a>
-                <hr/>
+                <hr />
 
 
                 <div>
@@ -62,7 +63,7 @@ export const Resume = () => {
                 </div>
 
             </motion.div>
-
+            <Footer />
         </section>
 
     )
