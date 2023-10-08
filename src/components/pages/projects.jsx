1950dabc8bf878350e57a2c3e43608a465b43b81
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-08 09:09:25 -0700

    list fix

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -14,72 +14,61 @@ const Body = () => {
                                     THIS WEBSITE (REACT/JS)
                                 </a>
                             </li>
-                            <br />
                             <li>
                                 <a href="https://github.com/austinhutchen/brdboard-companion.git" target="_blank"
                                     rel="noopener noreferrer">
                                     BREADBOARD MACRO PAD (C)
                                 </a>
                             </li>
-                            <br />
                             <li>
                                 <a href="https://github.com/austinhutchen/IOS-calculator-app" target="_blank"
                                     rel="noopener noreferrer">
                                     CALCULATOR APP (FLUTTER/DART)
                                 </a>
                             </li>
-                            <br />
                             <li>
                                 <a href="https://github.com/austinhutchen/weather-native.git" target="_blank"
                                     rel="noopener noreferrer">
                                     STATEFUL WEATHER APP (REACT NATIVE)
                                 </a>
                             </li>
-                            <br />
 
                             <li>
                                 <a href="https://github.com/austinhutchen/map-app.git" target="_blank" rel="noopener noreferrer">
                                     GOOGLE MAPS CLONE (Flutter/C++)
                                 </a>
                             </li>
-                            <br></br>
                             <li>
                                 <a href="https://github.com/austinhutchen/notable.git" target="_blank" rel="noopener noreferrer">
                                     JOURNALLING APP (REACT NATIVE/JAVA)
                                 </a>
                             </li>
-                            <br />
                             <li>
                                 <a href="https://github.com/austinhutchen/redditsearch" target="_blank" rel="noopener noreferrer">
                                     REDDIT STATISTICS ENGINE (PYTHON)
                                 </a>
                             </li>
-                            <br />
                             <li>
                                 <a href="https://github.com/austinhutchen/shell.git" target="_blank" rel="noopener noreferrer">
                                     COMPLETE KERNEL SHELL (VANILLA C)
                                 </a>
                             </li>
-                            <br />
                             <li>
                                 <a href="https://github.com/austinhutchen/FASTmatrixmult-c.git" target="_blank" rel="noopener noreferrer">
                                     STRAUSSENS MATRIX_MULTIPLY DIVIDE AND CONQUER ALGORITHM (C)
                                 </a>
                             </li>
-                            <br />
                             <li>
                                 <a href="https://github.com/austinhutchen/euclidshortest-c.git" target="_blank"
                                     rel="noopener noreferrer">
                                     N-COORDINATE SHORTEST DISTANCE DIVIDE + CONQUER ALGORITHM (C)
                                 </a>
                             </li>
-                            <br />
                             <li>
                                 <a href="https://github.com/austinhutchen/GAMMA.JS.git" target="_blank" rel="noopener noreferrer">
                                     2-3 DIMENSIONAL GRAPHING CALCULATOR (JAVASCRIPT)
                                 </a>
                             </li>
-                            <br />
 
                         </b>
                     </ul>
@@ -100,10 +89,10 @@ export const Projects = () => {
                 <h2 className="hlight" style={{ paddingTop: 20 }}>
                     <b>PERSONAL PROJECTS:</b>
                 </h2>
-                <hr></hr>
+                <br/>
                 <CommonCarousel />
                 <Body />
-                <hr></hr>
+                <br/>
 
             </motion.div>
         </section>
