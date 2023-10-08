629bb150eaf71c1e79b981573222a9d02b09a277
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-08 09:43:17 -0700

    accredation additional info

--- src/components/pages/accredations.jsx
+++ src/components/pages/accredations.jsx
@@ -7,7 +7,7 @@ export const View = (props) => {
     return (
         <section className="App">
             <Nav />
-            <h1 className="hlight"><b><i><u>Letter of Rec from MATH PHD @CU:</u></i></b></h1>
+            <h1 className="hlight"><b><i><u>Letter of Rec from MATH PHD students @ University Of Colorado Boulder:</u></i></b></h1>
             <PDFViewer data={props.data} />
             <PDFViewer data={''} />
             <Footer />
