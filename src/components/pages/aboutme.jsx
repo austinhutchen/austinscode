3abd538d15906e09d3c49e33c2d531031389f575
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-10 10:13:57 -0700

    better formatting

--- src/components/pages/aboutme.jsx
+++ src/components/pages/aboutme.jsx
@@ -3,16 +3,20 @@ import { Nav } from "../common/navbar"
 import React, { Component } from "react";
 
 import { motion } from "framer-motion"; 
-
+import me from "../images/me4.jpg";
+import coding from "../images/coding.JPG";
+import family from "../images/family.jpeg";
+import nature from "../images/nature.jpg";
+import mimi from "../images/mimi.jpg";
 import { Footer } from "../common/footer"
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import "../../css/dimensions.css";
 export const Aboutme = () =>
  {
     return (
-      <motion.div initial={{ width: 0 }} animate={{ width: "95%" }}
+      <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
                 exit={{ x: window.innerWidth, transition: { duration: 0 } }}>
             <Nav />
                 <Display  />
@@ -30,11 +34,11 @@ super();
             
           ],
             sliders: [
-              '/images/me4.jpg',
-                "/images/coding.JPG",
-                "/images/family.jpeg",
-                "/images/mimi.jpg",
-              "/images/nature.jpg"]
+             me,
+               coding,
+                family,
+                mimi,
+              nature]
         }
 
 
@@ -55,12 +59,11 @@ super();
       dots: true,
       infinite: true,
       speed: 500,
-        slidesToShow: 2,
+        slidesToShow: 1,
       slidesToScroll: 1,
 
     };
-    return (
-      <div className="slider-container" style={{paddingTop: 10}}> 
+    return (<div> 
 <h1 className="hlight">
               About me!
               </h1>
