5efcaaedb59c4a8f8494557245e972abf51afad6
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-10 07:44:32 -0700

    update

--- src/components/pages/aboutme.jsx
+++ src/components/pages/aboutme.jsx
@@ -25,10 +25,12 @@ export const Aboutme = () => {
 export const Display = (props) => {
     const arr = [props.img1, props.img2, props.img3, props.img4, props.img5, props.img6, props.img7, props.img8]
     let { height, width } = useWindowDimensions();
-    let  width_sldr=height<<2   ;
-  let width_itm = width << 2;
-  let img_height = height >>1;
-    return (
+   // media query for area of height and width
+  let vA = height * width ;
+
+  let width_itm = vA >> 2;
+  let img_height = vA >> 11;
+    return( 
         <div >
             <section style={{ paddingBottom: 10 }}>
                 <h1 className="hlight"  >
@@ -36,31 +38,31 @@ export const Display = (props) => {
                 </h1>
             </section>
       <div className="slider-container">
-            <Carousel sliderWidth={width_sldr}
+            <Carousel sliderWidth={width}
                 itemWidth={width_itm}>
                 <div>
-                    <img src={arr[0]} height={img_height} width='auto' />
+                    <img src={arr[0]} height={img_height} width='auto' loading="lazy" />
                     <p className="legend">  My name is <i><b></b>Austin Hutchen</i>. I'm a software engineer with aspirations of working on embedded systems and machine learning, and I have plenty of hobbies! Swipe for more:
                     </p>
                 </div>
                 <div>
-                    <img src={arr[4]} height={img_height} width="auto" />
+                    <img src={arr[4]} height={img_height} width="auto" loading="lazy" />
                     <p className="legend">I love coffee and coding!</p>
                 </div>
                 <div>
-                    <img src={arr[2]} height={img_height} width="auto" />
+                    <img src={arr[2]} height={img_height} width="auto" loading="lazy"/>
                     <p className="legend">I love quality family time!</p>
                 </div>
                 <div>
-                    <img src={arr[3]} height={img_height} width="auto" />
+                    <img src={arr[3]} height={img_height} width="auto" loading="lazy" />
                     <p className="legend">I love hiking!</p>
                 </div>
                 <div>
-                    <img src={arr[1]} height={img_height} width="auto" />
+                    <img src={arr[1]} height= {img_height} width="auto" loading="lazy" />
                     <p className="legend">I love cats!</p>
                 </div>
             </Carousel>
         </div>
         </div>
-    )
+    );
 }
