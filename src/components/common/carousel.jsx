35798450717b98f57b248738ac9c56a33d3059c3
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-10 09:35:44 -0700

    code fixes

--- src/components/common/carousel.jsx
+++ src/components/common/carousel.jsx
@@ -1,16 +1,11 @@
 
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import useWindowDimensions from '../base/optimize'
-import React  from 'react';
-
+import {React, Component}  from 'react';
+import Slider from 'react-slick' 
 export const CommonCarousel=()=>{
  // dimensions here
 return(
  <>
- <Carousel>
 
- </Carousel>
  
  
  
@@ -19,3 +14,56 @@ return(
 )
 
 }
+export default class Display extends Component  
+{
+  constructor(){
+super();
+        this.state = {
+          key:[
+            
+          ],
+            sliders: [
+              '../images/me4.jpg',
+                "../images/coding.JPG",
+                "../images/family.jpeg",
+                "../images/mimi.jpg",
+              "../images/nature.jpg"]
+        }
+
+
+  }
+            
+    sliders() {
+        return this.state.sliders.map(data => {
+            return (
+           
+                <div key={data}>
+                    <img alt="key" src={data} />
+                </div>
+                         )
+      });
+    }
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+        slidesToShow: 2,
+      slidesToScroll: 1,
+
+    };
+    return (
+      <div className="slider-container" style={{paddingTop: 10}}> 
+<h1 className="hlight">
+              About me!
+              </h1>
+        <Slider {...settings}> 
+      
+                    {this.sliders()}
+                </Slider>     
+
+      </div>
+    );
+  }
+}
+
