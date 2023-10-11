4f26fce7f29b00290945ef107c3f01175e51b4de
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-11 15:05:27 -0700

    d

--- src/components/pages/aboutme.jsx
+++ src/components/pages/aboutme.jsx
@@ -1,6 +1,6 @@
 import { Nav } from "../common/navbar"
 import React, { Component } from "react";
-import { motion } from "framer-motion"; 
+import { motion } from "framer-motion";
 import me from "../images/me4.jpg";
 import coding from "../images/coding.JPG";
 import family from "../images/family.jpeg";
@@ -11,85 +11,84 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../css/dimensions.css";
 import "../../css/fonts.css";
-export const Aboutme = () =>
- {
-    return (
-      <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
-                exit={{ x: window.innerWidth, transition: { duration: 0 } }}>
-            <Nav />
-                <Display  />
-        </motion.div>
-    )
+export const Aboutme = () => {
+  return (
+    <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
+      exit={{ x: window.innerWidth, transition: { duration: 0 } }}>
+      <Nav />
+      <Display />
+    </motion.div>
+  )
 }
 
 
-export default class Display extends Component  
-{
-  constructor(props){
-super(props);
-        this.state = {
-           user:""
-       }
-
+export default class Display extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: ""
     }
+
+  }
 
   render() {
 
-    var keys=[
-            {
-              desc:"My name is Austin Hutchen. I'm a software engineer with aspirations of working on embedded systems and machine learning, and I have plenty of hobbies! Swipe for more:",
-              url:me
-            }
-            ,
-            {
-              desc:             "I love coffee and coding!",
-              url:coding
-            },
-            {
-              desc:             "I love quality family time!",
-              url:family
-            },
-            {
-              desc:    "I love cats!",
-              url:mimi
-            },
-            {
-              desc:  "I love hiking!",
-              url:nature
-            }
-         
-           
-          ] ;
-   const settings = {
+    var keys = [
+      {
+        desc: "My name is Austin Hutchen. I'm a software engineer with aspirations of working on embedded systems and machine learning, and I have plenty of hobbies! Swipe for more:",
+        url: me
+      }
+      ,
+      {
+        desc: "I love coffee and coding!",
+        url: coding
+      },
+      {
+        desc: "I love quality family time!",
+        url: family
+      },
+      {
+        desc: "I love cats!",
+        url: mimi
+      },
+      {
+        desc: "I love hiking!",
+        url: nature
+      }
+
+
+    ];
+    const settings = {
       dots: true,
       infinite: true,
       speed: 150,
-        slidesToShow: 1,
+      slidesToShow: 1,
       slidesToScroll: 1,
-lazyLoad:true,
-   centerMode: true,
+      lazyLoad: true,
+      centerMode: true,
 
     };
-       return (<div> 
-<h1 className="hlight" >
-              About me!
-              </h1>
-         <br/>
-         <br/>
-          <div className="sc">
-        <Slider {...settings}>    
-         {keys.map( data =>{   
-             return(
-               <div className="slick-slide">
-                <h2> 
-             {data.desc}
-               </h2>
-               <img src={data.url}/>
-               </div>
-             ); }) }                                  
-                </Slider>     
-     </div>
+    return (<div>
+      <h1 className="hlight" >
+        About me!
+      </h1>
+      <br />
+      <br />
+      <div className="sc">
+        <Slider {...settings}>
+          {keys.map(data => {
+            return (
+              <div className="slick-slide">
+                <h2>
+                  {data.desc}
+                </h2>
+                <img src={data.url} />
+              </div>
+            );
+          })}
+        </Slider>
       </div>
+    </div>
     );
   }
 }
