731a9bd309c9dae1bccae6ba04152f0f17c32caa
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-12 11:53:57 -0700

    motion div animation wrapper for accredations page added

--- src/components/pages/accredations.jsx
+++ src/components/pages/accredations.jsx
@@ -1,16 +1,18 @@
 import { Nav } from "../common/navbar";
 import { PDFViewer } from "../common/PDFviewer";
 import { Footer } from "../common/footer";
+import { motion } from "framer-motion"; 
 import React  from 'react';
 
 export const View = (props) => {
     return (
-        <section className="App">
+           <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
+            exit={{ x: window.innerWidth, transition: { duration: 0 } }}>
             <Nav />
             <h1 className="hlight"><b><i><u>Letter of Rec from MATH PHD students @ University Of Colorado Boulder:</u></i></b></h1>
             <PDFViewer data={props.data} />
             <PDFViewer data={''} />
             <Footer />
-        </section>
+        </motion.div>
     )
 }
