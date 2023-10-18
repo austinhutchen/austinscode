02a0baf8c8cf977f19d5fa12152dfb756851e957
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-18 11:20:49 -0700

    fix

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -7,7 +7,7 @@ export const Nav = () => {
     return (
         <div>
             <nav
-                class="navbar navbar-dark bg-dark fixed"
+                class="navbar navbar-dark bg-dark navbar-fixed-top"
             >
                 <div>
                     <button
