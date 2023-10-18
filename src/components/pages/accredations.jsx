ecbd598517c932e29e9132af6bd32f753e5e5f8b
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-18 08:54:15 -0700

    fix

--- src/components/pages/accredations.jsx
+++ src/components/pages/accredations.jsx
@@ -1,18 +1,20 @@
 import { Nav } from "../common/navbar";
 import { PDFViewer } from "../common/PDFviewer";
 import { Footer } from "../common/footer";
-import { motion } from "framer-motion"; 
-import React  from 'react';
+import { motion } from "framer-motion";
+import React from 'react';
 
 export const View = (props) => {
     return (
-           <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
+        <>
+        <Nav />
+        <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
             exit={{ x: window.innerWidth, transition: { duration: 0 } }}>
-            <Nav />
             <h1 className="hlight"><b><i><u>Letter of Rec from MATH PHD students @ University Of Colorado Boulder:</u></i></b></h1>
             <PDFViewer data={props.data} />
             <PDFViewer data={''} />
             <Footer />
         </motion.div>
+        </>
     )
 }
