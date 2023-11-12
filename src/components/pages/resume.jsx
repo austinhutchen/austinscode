6963688cce12f818f9e8a8fda4a2d240643342df
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-12 12:07:09 -0800

    font fixes

--- src/components/pages/resume.jsx
+++ src/components/pages/resume.jsx
@@ -18,13 +18,13 @@ const Body = () => {
                     <b> DIRECT WORK EXPERIENCE </b>
                 </h1>
       <ul >
-  <h3>
+  <h3 className="hlight">
                     <b>
                         <u>Calculus 2 Course Assistant (University Of Colorado Boulder) (January 2023 - May 2023)</u>
                     </b>
                 </h3>
                 <li>
-                 < p>
+                 < h3>
                     <b >
                         Taught and assisted with CALCULUS 2 (MATH 2300) alongside faculty at CU boulder. Also held office
                         hours every week to assist learning of course material.{" "}
@@ -32,63 +32,57 @@ const Body = () => {
                         Recieved 97% positive feedback from 103 participants on end of
                         semester survey.
                     </b>
-                 </p>
+                 </h3>
                 </li>
 
-                <h3>
+                <h3 className="hlight">
                     <b>
                         <u>Mobius Materials, Marketing Consultant (Summer 2020-Fall 2020)</u>
                     </b>
                 </h3>
                 <li>
-                    <p className="projdesc">
+                    <h3 className="projdesc">
                         <b>
                             {" "}
                             Made first company sale as Marketing consultant, worked closely with
                             the software engineering team to achieve forecasted results.{" "}
                         </b>
-                    </p>
+                    </h3>
                 </li>
-                              <h4>
+                              <h3 className="hlight">
                     <b>
                         <u>Ebay Electronics Repairs + Reseller (May 2018 -  Now 2023)</u>
                     </b>
-                </h4>
+                </h3>
 <li>
-                <p>
-                    <b>
+                <h3 className="projdesc">
                         I sell refurbished and custom-built Apple, Linux, and Windows electronics, have handled over 50 total customer sales to-date with 100% positive feedback rate from 45 customers on ebay. Also handle macbook, iphone, and ipad repairs, as well as Windows PC build requests. You can read the reviews and find my account by <b> <i> <u> clicking the image below: </u></i></b>
+</h3>
 
-                        <h4>
+
                             <a href="https://www.ebay.com/usr/gadgetsllcc"  target="_blank"
                 rel="noreferrer" className="referral">
 <img src ={ebayicon} className= "icon"/>
                             </a>
-                        </h4>
-
-                    </b>
-                </p>
+                        
 
 </li>
       <li>
-   <h4>
+   <h3 className="hlight">
                     <b>
                         <u>Github  Open Source Developer(May 2022 -  Now 2023)</u>
                     </b>
-                </h4>
+                </h3>
 </li>
 <li>
-                    <b>
-                       I contribute to various open source projects and build many of my own, which you can see displayed in detail on my projects page. Of those I have contributed to are Twitter, The Official CU Boulder MATH Github, and<i><u> much more below.</u></i> </b>
-                    <b>
-                        <h4>
+    <h3 className="projdesc">
+                       I contribute to various open source projects and build many of my own, which you can see displayed in detail on my projects page. Of those I have contributed to are Twitter, The Official CU Boulder MATH Github, and<b><i><u> much more below.</u></i> </b>
+                        </h3>
                             <a href= "https://github.com/austinhutchen"  target="_blank"
                 rel="noreferrer">
      <img src={ghicon} className="icon" /> 
                             </a>
-                        </h4>
 
-                    </b>
                 </li>
 
       
