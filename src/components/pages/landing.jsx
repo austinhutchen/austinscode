98e8cb93667c08e60cbe709ef106ea1a0594cfaf
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-14 06:04:55 -0800

    css cleanup

--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -4,6 +4,8 @@ import { Spinner } from '../common/symbols';
 import { Footer } from '../common/footer';
 import React, { useEffect, useRef } from "react";
 import TagCloud from "TagCloud";
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from "react-scroll"; 
 import "../../css/TextSphere.css";
 const Head = () => {
   return (
@@ -44,9 +46,8 @@ const TextSphere = () => {
       "and more!.."
     ];
     const options = {
-      radius: 280,
-      maxSpeed: "high",
-      initSpeed: "low",
+      radius: 250,
+      initSpeed: "high",
       keep: true,
       loop: true,
       lockX: true,
@@ -90,12 +91,32 @@ export const Landing = () => {
   return (
     <>
    <Nav />
+ <h3 className="text-4xl sm:text-7xl text-white" style={{paddingTop:10}} className="hlight">
+         <b> Welcome to my website! I'm Austin, and I'm a Full Stack Web Developer. </b>
+        </h3>
    <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
             exit={{ x: window.innerWidth, transition: { duration: 0.001  } }}>
-      
-    <h3>     <b> Full-Stack Embedded Software Engineer & CS student with passion for development and experience working with startups! </b></h3>
-<h5 className="subtext"> <i><b> Through building my projects, I have learned and practiced rigorously the topics below. </b> </i> </h5>
+       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="flex flex-col justify-center h-full">
 
+        <p className="text-gray-500 py-4 max-w-md">
+           I have 4 years of experience in web development, backend server development, and more recently, embedded systems development.
+          Currently, I love to work on web application using technologies like
+          React, Tailwind, Next.js and Mongodb.
+        </p>
+          <Link
+            to="about"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+          >
+            About Me
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight size={25} className="ml-3" />
+            </span>
+          </Link>
+        </div>
+      </div>
       <Body />
     
 </motion.div>
