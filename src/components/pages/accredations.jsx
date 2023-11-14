b78acd84af70984cba8cfa0911cb91d67ef2b548
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-14 13:53:03 -0800

    pdf viewer mobile opt

--- src/components/pages/accredations.jsx
+++ src/components/pages/accredations.jsx
@@ -19,7 +19,7 @@ export const View= (props) => {
             exit={{ x: window.innerWidth, transition: { duration: 0.001  } }}>
 <section>
             <h1 className="hlight"><b><i><u>Letter of Rec from MATH PHD students @ University Of Colorado Boulder:</u></i></b></h1>
-   <iframe src={props.data+"#zoom=FitW"} className="pdf" height="100%" width="100%" frameborder='0' allowfullscreen></iframe>
+   <iframe src={props.data+"#view=FitH"} className="pdf" height="100%" width="100%" frameborder='0' allowfullscreen></iframe>
       </section>
 <section>
    <h1 className="hlight"><b><i><u>Student comments from my CALCULUS 2 / MATH 2300 sections @ University Of Colorado Boulder:</u></i></b></h1>
