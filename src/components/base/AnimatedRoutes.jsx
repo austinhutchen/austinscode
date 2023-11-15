cf41ee5bb85bf6b3ad86d76460d47b6f7d6767c8
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-15 11:04:40 -0800

    even faster images

--- src/components/base/AnimatedRoutes.jsx
+++ src/components/base/AnimatedRoutes.jsx
@@ -7,8 +7,22 @@ import { Aboutme } from "../pages/aboutme"
 import { AnimatePresence } from "framer-motion"; 
 import _pdf from "../pages/images/REC_LETTER.png"
 import _pdf2 from "../pages/images/RESUME_SWE.png"
+import me from "./fast_imgs/me4.webp";
+import coding from "./fast_imgs/coding.webp";
+
+import systems from "./fast_imgs/systems.webp";
+
+
+import math from "./fast_imgs/IMG_0913.webp";
+import family from './fast_imgs/family.webp';
+import mimi from "./fast_imgs/mimi.webp";
+import nature from "./fast_imgs/nature.webp";
+
+
+
 import React  from 'react';
 export let AnimatedRoutes = () => {
+  const aboutme_props = [me,coding,systems,math,family,mimi,nature]
     const location = useLocation();
     return (
         <div className="main-wrapper">
@@ -19,7 +33,7 @@ export let AnimatedRoutes = () => {
                         <Route exact path="src/components/landing.js" element={<Landing />} />
                         <Route exact path="src/components/resume.js" element={<Resume data={_pdf2} />} />
                         <Route exact path="src/components/projects.js" element={<Projects />} />
-                        <Route exact path="src/components/aboutme.js" element={<Aboutme />} />
+                        <Route exact path="src/components/aboutme.js" element={<Aboutme data={aboutme_props} />} />
                         <Route exact path="src/components/accredations.js" element={<View data={_pdf} />} />
                     </Routes>
                 </AnimatePresence>
