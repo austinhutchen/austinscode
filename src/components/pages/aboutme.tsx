92cc19305a7cd7b2c167ae9a1ce8ab397eedfe01
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-16 22:04:59 -0800

    d

--- src/components/pages/aboutme.tsx
+++ src/components/pages/aboutme.tsx
@@ -38,33 +38,44 @@ export default class Display extends Component {
     var keys = [
       {
         desc: "My name is Austin Hutchen. I'm a software engineer with aspirations of working on embedded systems and machine learning, and I have plenty of hobbies! Swipe for more: ",
-        url: this.props.data[0] 
+        url: this.props.data[0],
+        loading:"eager"
+
       }
       ,
       {
         desc: "In my free time, I love to optimize code! I practice leetcode frequently to refine my understanding of algorithms, P/NP runtimes and read often on the problems of code portability and optimization, especially in languages like C (~My favorite~). ",
-        url: this.props.data[1]
+        url: this.props.data[1],
+        loading:"eager"
       },
   {
         desc: "I love embedded systems!",
-        url: this.props.data[2]
+        url: this.props.data[2],
+        loading:"eager"
       },
       {
         desc: "I love Mathematics!",
-        url: this.props.data[3]
+        url: this.props.data[3],
+        loading: "lazy"
       }
       ,
       {
         desc: "I love quality family time!",
-        url: this.props.data[4]
+        url: this.props.data[4],
+        loading: "lazy"
+
       },
       {
         desc: "I love cats!",
-        url: this.props.data[5]
+        url: this.props.data[5],
+        loading: "lazy"
+
       },
       {
         desc: "I love hiking!",
-        url: this.props.data[6]
+        url: this.props.data[6],
+        loading: "lazy"
+
       },
     
 
@@ -94,7 +105,7 @@ export default class Display extends Component {
           {keys.map(data => {
             return (
               <div className="slick-slide" >
-                <img src={data.url} style={{borderRadius: 450/ 2, borderStyle:"solid", border: "0.8vh solid #0BA" }}/>
+                <img src={data.url} loading={data.loading} style={{borderRadius: 450/ 2, borderStyle:"solid", border: "0.6vh solid #0BA" }} />
 
                             <fieldset >
               <p className ="projdesc" style={{color:'cornsilk' ,  fontSize:"0.95em",  fontFamily: "Gill Sans, Gill Sans MT,Calibri,Trebuchet MS,sans-serif"   , fontWeight: 350,textShadow: "2px 3px 5px rgba(0,0,0,0.5)"}}>
