1c36684daec9449ad054fe7fcac5b732fb92aeef
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-21 05:55:43 -0700

    image fix

--- src/components/pages/accredations.tsx
+++ src/components/pages/accredations.tsx
@@ -1,13 +1,11 @@
 import { Nav } from "../common/navbar.tsx";
-import {Suspense} from "react";
-// Plugins
 import { motion } from "framer-motion";
 
 
 
 export const View= (props) => {
     return (
-        <div>
+        <>
         <Nav />
 
         <motion.div initial={{ width: 0 }} animate={{ width: "100%" }}
@@ -16,27 +14,25 @@ export const View= (props) => {
 <section>
             <h1 className="hlight"><b><i><u>Letter of Rec from MATH PHD students @ University Of Colorado Boulder:</u></i></b></h1>
       <br/>
-     <img src ={props.data[0]} className="format-img"/> 
+     <img src ={props.data[0]} className="format-img" loading ="eager"/> 
 
       </section>
-<>
+
+   </motion.div>
    <h1 className="hlight"><b><i><u>Student comments from my CALCULUS 2 / MATH 2300 sections @ University Of Colorado Boulder:</u></i></b></h1>
       <br/>
 
       <br/>
-      <Suspense fallback={<p> loading.. </p>}>
       <img src= {props.data[1]} className="format-img" loading ="lazy"/>
       <img src= {props.data[2]} className="format-img"  loading ="lazy"/>
       <img src= {props.data[3]} className="format-img" loading ="lazy" />
-      </Suspense>
-      </>
-   </motion.div>
 
 
 
 
 
-        </div>
+
+        </>
     )
 }
 
