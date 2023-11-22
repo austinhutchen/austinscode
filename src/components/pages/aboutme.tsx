4e8615911ed95f3104239237b092764b39c6b7a1
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-22 11:16:04 -0700

    d

--- src/components/pages/aboutme.tsx
+++ src/components/pages/aboutme.tsx
@@ -1,4 +1,5 @@
 
+import React from 'react'
 import { Nav } from "../common/navbar.tsx"
 import { Component } from "react";
 import { motion } from "framer-motion";
@@ -30,52 +31,46 @@ export const Aboutme = (props) => {
 }
 
 
-export default class Display extends Component {
-
-
-
-  render(props) {
-
-    const keys = [
+ const Display =(props)=>{
+   const keys = [
       {
         desc: "Welcome! I'm a software engineer with aspirations of working on embedded systems and machine learning, and I have plenty of hobbies! ",
-        url: this.props.data[0],
+        url: props.data[0],
       }
       ,
       {
         desc: "In my free time, I love to optimize code! I practice leetcode frequently to refine my understanding of algorithms, P/NP runtimes and read often on the problems of code portability and optimization, especially in languages like C (~My favorite~). ",
-        url: this.props.data[1],
+        url: props.data[1],
       },
       {
         desc: "I love embedded systems!",
-        url: this.props.data[2],
+        url: props.data[2],
       },
       {
         desc: "I love Mathematics!",
-        url: this.props.data[3],
+        url: props.data[3],
       }
       ,
       {
         desc: "I love quality family time!",
-        url: this.props.data[4],
+        url: props.data[4],
 
       },
       {
         desc: "I love cats!",
-        url: this.props.data[5],
+        url: props.data[5],
 
       },
       {
         desc: "I love hiking!",
-        url: this.props.data[6],
+        url: props.data[6],
 
       },
 
 
     ];
 
-
-    const settings = {
+   const settings = {
       dots: true,
       slidesToShow: 1,
       centerMode: true,
@@ -86,16 +81,18 @@ export default class Display extends Component {
       cssEase: 'cubic-bezier(0.445, 0.05, 0.55, 0.95)',
       adaptiveHeight: true,
     }
+    const slider = React.useRef(null);
     return (
       <div className="sc">
-        <Slider {...settings} >
+        <Slider ref ={slider} {...settings} >
 
           {keys.map(data => {
             return (
 
               <>
                 <img src={data.url} style={{ borderRadius: 200 / 2, borderStyle: "solid", fontSize: '1.2em', border: "0.5vh solid #0BA" }} />
-
+<button onClick={() => slider?.current?.slickPrev()}>Prev</button>
+<button onClick={() => slider?.current?.slickNext()}>Next</button>
                 <fieldset >
                   <p style={{ color: 'cornsilk', fontSize: "0.95em", paddingTop: '0.8em', fontFamily: "Gill Sans, Gill Sans MT,Calibri,Trebuchet MS,sans-serif", fontWeight: 350, textShadow: "2px 3px 5px rgba(0.8,0.8,0.8,0.8)" }}>
 
@@ -116,5 +113,7 @@ export default class Display extends Component {
         </Slider>
       </div>
     );
-  }
+
 }
+
+
