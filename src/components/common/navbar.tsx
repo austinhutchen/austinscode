662cd92f61b7b050a5fb59c53a979bd23a46a17c
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-22 08:32:26 -0700

    d

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -25,7 +25,7 @@ export const Nav = () => {
                         type="button"
                         id="navbar"
                         onClick={() => {
-                            nav('/Landing');
+                            nav('/');
                         }}
                     >
                         Home
