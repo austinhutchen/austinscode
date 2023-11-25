fa98c59b1794d54946eb5eb375237aa1f60da55e
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-25 10:52:25 -0700

    f

--- src/components/pages/projects.tsx
+++ src/components/pages/projects.tsx
@@ -37,7 +37,7 @@ const Body = (props) => {
 
                     <img src={props.data[9]} height="140vh" width="auto"  />
 
-                    <h4> Used a DHT11 serial temperature & humidity sensor along with a 7 segment display (displaying 7-bit I2C transmission to ESP32) with 9V of portable battery power to display real-time information over wifi to a react app. Includes data correction for imprecise measurements. Embedded instructions coded fully in c using the platformio platform, and app was coded using typescript, scss, and the react native framework.</h4>
+                    <h4> Used a DHT11 serial temperature & humidity sensor along with a 7 segment display (displaying 7-bit I2C transmission to ESP32) with 9V of portable battery power to display real-time information over wifi to a react app. Includes a custom thermistor-checked data correction algorithm on the DHT11 for fixing imprecise temperature measurements. Embedded instructions coded fully in c using the platformio platform, and app was coded using typescript, scss, and the react native framework.</h4>
                 </li>
 
      <li>
