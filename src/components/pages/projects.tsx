08137dc6731303d4207014a097129ca63b7e3a93
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-06 10:46:42 -0800

    mobile opt

--- src/components/pages/projects.tsx
+++ src/components/pages/projects.tsx
@@ -4,7 +4,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../css/dimensions.scss";
 import "../../css/fonts.scss";
-import '../../css/projects.scss'
 import "../../css/QUERIES.scss"
 import React from "react"
 const Body = (props) => {
