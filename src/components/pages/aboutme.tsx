020cf2ef0be750942604942288ed406eb945bf4f
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-06 08:56:57 -0800

    iphone fix

--- src/components/pages/aboutme.tsx
+++ src/components/pages/aboutme.tsx
@@ -110,7 +110,7 @@ return(
 
             <div>
               { (data.type == "image")?<img src={data.url} style={{ borderRadius: '(200 / 2)em', borderStyle: "solid", border: "0.45vh solid #0BA" }} /> :<>
-                 <video controls loop playsinline autoPlay muted height="auto" width="auto" preload="metadata" style={{ borderRadius: '(200 / 2)em', borderStyle: "solid", border: "0.45vh solid #0BA" }} >
+                 <video controls autoPlay loop playsInLine  muted height="auto" width="auto" preload="metadata" style={{ borderRadius: '(200 / 2)em', borderStyle: "solid", border: "0.45vh solid #0BA" }} >
                         <source src={ data.url+ "#t=0.5"} type="video/mp4" />
                     </video>
                     <br/>
