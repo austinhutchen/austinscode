e61e380e6d654a44bc59bc973116841529aef76c
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-01-18 09:58:56 -0800

    d

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import "../../App.css"
+import "../../App.scss"
 export const Nav: React.FC = () => {
   const nav = useNavigate();
 
