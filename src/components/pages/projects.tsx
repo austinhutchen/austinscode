a7d43199a1a789115e90a00647ee265d99dcac11
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-01-20 04:27:23 -0800

    d

--- src/components/pages/projects.tsx
+++ src/components/pages/projects.tsx
@@ -1,38 +1,35 @@
+import React, { memo } from "react";
 import { LazyMotion, domAnimation, m } from "framer-motion";
-
-import React, { memo } from "react"
 import { Nav } from "../common/navbar.tsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../css/dimensions.scss";
 import "../../css/fonts.scss";
-import "../../css/QUERIES.scss"
+import "../../css/QUERIES.scss";
 
-const shell = process.env.PUBLIC_URL + '/fast_imgs/shell.webp';
-const calculator = process.env.PUBLIC_URL + '/fast_imgs/calculator.webp';
-const maps = process.env.PUBLIC_URL + '/fast_imgs/maps.webp';
-const gesture = process.env.PUBLIC_URL + '/fast_imgs/gesture.mp4';
-const PLANT_WATERER = process.env.PUBLIC_URL + '/fast_imgs/PLANT_KERNEL.mp4';
-const encryptc = process.env.PUBLIC_URL + '/fast_imgs/encryptc.webp';
-const wled2 = process.env.PUBLIC_URL + '/fast_imgs/WLED2.mp4';
-const my_interface = process.env.PUBLIC_URL + '/fast_imgs/interface.webp';
+const getImgPath = (imageName) => `${process.env.PUBLIC_URL}/fast_imgs/${imageName}`;
 
-const notable = process.env.PUBLIC_URL + '/fast_imgs/journalapp.webp';
-
-const weather = process.env.PUBLIC_URL + '/fast_imgs/WEATHER.webp';
-const butterfly = process.env.PUBLIC_URL + '/fast_imgs/animate.mp4';
-const graph = process.env.PUBLIC_URL + '/fast_imgs/graph.mp4';
-const WEATHER_SENSOR = process.env.PUBLIC_URL + '/fast_imgs/ESP_WEATHER.webp';
-const search = process.env.PUBLIC_URL + '/fast_imgs/PYCLI.webp';
-const actix = process.env.PUBLIC_URL + '/fast_imgs/concurrency-model.webp';
-const bash = process.env.PUBLIC_URL + '/fast_imgs/BASH.webp';
-const php = process.env.PUBLIC_URL + '/fast_imgs/PHP.webp';
-const algos = process.env.PUBLIC_URL + '/fast_imgs/ALGOS.PNG';
-
-const game = process.env.PUBLIC_URL + '/fast_imgs/game1.webp';
-
-
-
+const images = {
+  shell: getImgPath('shell.webp'),
+  calculator: getImgPath('calculator.webp'),
+  maps: getImgPath('maps.webp'),
+  gesture: getImgPath('gesture.mp4'),
+  PLANT_WATERER: getImgPath('PLANT_KERNEL.mp4'),
+  encryptc: getImgPath('encryptc.webp'),
+  wled2: getImgPath('WLED2.mp4'),
+  my_interface: getImgPath('interface.webp'),
+  notable: getImgPath('journalapp.webp'),
+  weather: getImgPath('WEATHER.webp'),
+  butterfly: getImgPath('animate.mp4'),
+  graph: getImgPath('graph.mp4'),
+  WEATHER_SENSOR: getImgPath('ESP_WEATHER.webp'),
+  search: getImgPath('PYCLI.webp'),
+  actix: getImgPath('concurrency-model.webp'),
+  bash: getImgPath('BASH.webp'),
+  php: getImgPath('PHP.webp'),
+  algos: getImgPath('ALGOS.PNG'),
+  game: getImgPath('game1.webp'),
+};
 
 interface Project {
   title: string;
@@ -56,7 +53,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/FASTCALCAPP.c.git",
     description:
       "A calculator app with support for many different functions, such as factorials, transcendental functions like sin and cosine that utilized taylor series approximations for efficiency, and bit-level square root formulas for blazingly flast performance.  Written in C and programmed for IOS.",
-    images: [calculator],
+    images: ["calculator"],
     video: [],
 
   },
@@ -66,14 +63,14 @@ const projectsData: Project[] = [
     description:
       "Used a Sound-reactive low-power ESP8266, I2S MEMS microphone breakout & 16x16 LED matrix to display various animations using a WLED firmware flashed directly onto the esp8266. Images could be uploaded and display could be communicated with wirelessly over a wifi app, an HTTP proxy, or with an infrared reciever, and even respond with different bounce effects to music (Using the Fast Fourier Transform Signal Decomposition algorithm!)",
     images: [],
-    video: [wled2],
+    video: ["wled2"],
   },
   {
     title: "UNIX KERNEL SHELL (C/BASH)",
     link: "https://github.com/austinhutchen/shell.git",
     description:
       " A shell is the user's primary interface into any UNIX/OSX/WINDOWS operating system. Coding this provided me with a wealth of systems and UNIX knowledge. The kernel implemented killing and starting of processes, and functioned inside the user's local terminal.",
-    images: [shell],
+    images: ["shell"],
     video: [],
   },
   {
@@ -83,7 +80,7 @@ const projectsData: Project[] = [
       "A machine learning model implemented on an Arduino Nano BLE microcontroller that can sense and respond to various distinct Left-Right-Up-Down gestures. Trained on data provided by the arduino nano ble sense rev2 sensors.",
     images: [], // Insert your image paths here
 
-    video: [gesture],
+    video: ["gesture"],
 
   },
   {
@@ -91,7 +88,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/weather-sense.ts.git",
     description:
       "Used an ESP8266 & DHT11 serial temperature & humidity sensor along with a 7 segment display (displaying 7-bit I2C transmission to ESP32) with 9V of portable battery power to display real-time information over wifi to a react app, and display reading accuracy levels on the display. Includes a custom thermistor-checked data correction algorithm on the DHT11 for fixing imprecise temperature measurements. Embedded instructions coded fully in c using the platformio platform, and app was coded using typescript, scss, and the react native framework.",
-    images: [WEATHER_SENSOR, weather],
+    images: ["WEATHER_SENSOR", "weather"],
     video: [],
   },
   {
@@ -100,7 +97,7 @@ const projectsData: Project[] = [
     description:
       "My custom Plant Irrigation System, a cheap and power-efficient ESP-8266 powered system that reads in data from a custom soil sensor.It then checks the capacitive sensor moisture threshold and chooses whether or not to pump the water using a relay and water tube into the plant. I aim to upgrade this project with a solar-powered portable battery that can be switched off directly from the ESP-8266.",
     images: [], // Insert your image paths here
-    video: [PLANT_WATERER],
+    video: ["PLANT_WATERER"],
   },
 
   {
@@ -108,7 +105,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/levelquest.git",
     description:
       "One of my biggest projects, and also one of my first. Built a turn-based dungeon crawler with graphics embedded within a display matrix. Used to practice foundations of object-oriented programming and memory reference.",
-    images: [game],
+    images: ["game"],
     video: [],
 
   },
@@ -118,7 +115,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/map-app.git",
     description:
       "A Google Maps app clone that I coded using Flutter and the Dart framework. Spent the most time organizing and dealing with location data from Google's Maps API endpoint and building a usable interface similar to Google Maps.",
-    images: [maps], // Insert your image paths here,
+    images: ["maps"], // Insert your image paths here,
     video: [], // Insert your video path here,
 
   },
@@ -127,7 +124,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/austinscode",
     description:
       "I utilized bit masks to code byte-level animations for embedded systems using general 16x2 I2C interfacing LCDs. The animations utilized an ESP8266 frame buffer for displaying the images and saving temporarily into local memory. You can see the demo above.",
-    images: [butterfly], // Insert your image paths here
+    images: ["butterfly"], // Insert your image paths here
     video: [], // Insert your video path here
   },
 
@@ -136,7 +133,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/redditsearch.git",
     description:
       "A reddit search engine I built using an older version of python, that functioned using a binary search algorithm to quickly retrieve data at a user's query, such as the top post of the week, top users using a given search term, and many other options. Functioned entirely inside a virtual environment in the user's local terminal.",
-    images: [search],
+    images: ["search"],
     video: [],
   }
   ,
@@ -145,7 +142,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/notable.git",
     description:
       "An app made for note-sharing purposes, which utilized the AsyncStorage library to interface with iPhone and Android local cache storage. The app prompted the user with daily quotes fetched from an API and stored journal entries in the user's local cache system.",
-    images: [notable], // Insert your image paths here
+    images: ["notable"], // Insert your image paths here
     video: [], // Insert your video path here
   },
 
@@ -155,7 +152,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/encrypt.C.git",
     description:
       "A command line text editor interface, with local save as .txt and full file traversal capability using pointers. The program was able to also fully encrypt and decrypt into binary encodings from text input. I plan to use huffman encoding to also compress text and other(image, video) formats in the future. Coded fully in c.",
-    images: [encryptc], // Insert your image paths here
+    images: ["encryptc"], // Insert your image paths here
     video: [], // Insert your video path here
   },
 
@@ -165,7 +162,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/PHPland.git",
     description:
       "A collection of various PHP server scripting projects. Among them are a serving system for uploading data directly from an ESP32 into a MYSQLI database storage using PHP scripting, and a system for communicating data directly WITH an ESP32 using HTTP protocol and interpretation. PHP is one of my favorite languages.",
-    images: [php],
+    images: ["php"],
     video: [],
   },
 
@@ -173,7 +170,7 @@ const projectsData: Project[] = [
     title: "VARIOUS BASH SHELL SCRIPTS",
     link: "https://github.com/austinhutchen/scripts.git",
     description: "Coded using bash and kernel commands on mac OSX capable systems, I coded a VARIABLE-BIT PASSWORD GENERATOR, NMAP SCANNER, FILE TREE PRINTER, and SQL database initialization from within the CLI.",
-    images: [bash],
+    images: ["bash"],
     video: [],
   },
 
@@ -183,7 +180,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/austinscode",
     description:
       "Multi-threaded & secure newsletter delivery system coded from the ground up in rust, to accomodate the scaling of users on a quick ACTIX-WEB powered rust server backend. Extensive testing and test cases are packaged, and were used to practice building test cases for server-based tasks.",
-    images: [actix],
+    images: ["actix"],
     video: [], // Insert your video path here
 
   },
@@ -192,7 +189,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/CALGORITHMS.git",
     description:
       "A library I created to store data structures and algorithms that I use in my projects frequently or like to have on hand, coded by me from scratch using pointers in c++. Included is straussen's matrix multiplication algorithm, a fast-fourier-transform implementation, various implementations of hash tables with collision-checking, binary trees, graphs,and much more.",
-    images: [algos],
+    images: ["algos"],
     video: [],
   },
 
@@ -210,7 +207,7 @@ const projectsData: Project[] = [
     link: "https://github.com/austinhutchen/austinscode",
     description:
       "I used a 2.9-inch e-paper display and an ESP8266 directly with a 4x4 button matrix to display various on-screen paper-like animations (from 5 bit-masks displayed frame-by-frame), in the palm of your hand.",
-    images: [my_interface], // Insert your image paths here
+    images: ["my_interface"], // Insert your image paths here
 
     video: [], // Insert your video path here
 
@@ -221,7 +218,7 @@ const projectsData: Project[] = [
     description:
       "A graphing calculator coded in javascript which utilized a plotjs library to plot real-valued linear transformations on both a 2-dimensional and 3-dimensional domain/range, respectively. Helped to visualize many linear algebra concepts like rank, vector spaces, span, and determinants while I took the class. Seen in the video is the ability to also zoom in and localize the transformation, e.g. take the derivative of the function. A simple project, but one that I was grateful to put my love for math into nonetheless.",
     images: [], // Insert your image paths here
-    video: [graph], // Insert your video path here
+    video: ["graph"], // Insert your video path here
   },
 
 ];
@@ -242,23 +239,20 @@ const ProjectItem = memo(({ project }) => (
     </a>
     <br />
 
-    {project.images && project.images.map((src, imgIndex) => (
-      <>
-        <img src={src} key={imgIndex} height="145vh" width="auto" loading="eager" />
-	<br/>
-      </>
-    ))
-
-    }
-    {project.video && project.video.map((src, index) => (
-      <>
+    {project.images && project.images.map((imageName, imgIndex) => (
+      <React.Fragment key={imgIndex}>
+        <img src={images[imageName]} height="145vh" width="auto" loading="eager" />
+        <br />
+      </React.Fragment>
+    ))}
+    {project.video && project.video.map((videoName, videoIndex) => (
+      <React.Fragment key={videoIndex}>
         <video
-          key={index}
-          src={src}
+          src={images[videoName]}
           loop
           muted
           defaultMuted
-          autoplay
+          autoPlay
           playsInline
           loading="eager"
           onContextMenu={(e) => e.preventDefault()}
@@ -267,37 +261,23 @@ const ProjectItem = memo(({ project }) => (
           width="auto"
           preload="metadata"
         />
-
-      </>
+      </React.Fragment>
     ))}
 
-    <b>
-      {project.description}
-
-    </b>
+    <b>{project.description}</b>
   </li>
 ));
-
-
-
-
-
-
-
-
-
 
 export const Projects = () => (
   <div className="hlight-mini">
     <LazyMotion features={domAnimation}>
       <Nav />
-      <m.div initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0 } }} >
+      <m.div initial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth, transition: { duration: 0 } }}>
         <h1 className="hlight">
           <b>PERSONAL PROJECTS:</b>
         </h1>
 
         <ProjectList />
-
       </m.div>
     </LazyMotion>
   </div>
