2adbe605b047e8cd18d024d0fa0ed17b7d6dd812
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-02-21 12:44:12 -0800

    done

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -78,7 +78,7 @@ export const Landing: React.FC = () => {
           <div className="flex flex-col justify-center h-full">
             <b>
 <p className ="intro">
-I'm a Computer Science & Mathematics student at CU Boulder (BSc 2025) with a passion for eco-friendly software development, blockchain, network & embedded engineering, working with Artificial Intelligence, and teaching Calculus! I'm proficient in Typescript, PHP, C#, React(&Native), C++, Bash, and enjoy working with many other programming languages. I specialize in single-page application development, software code optimization, database management, and building embedded systems.
+I'm a Computer Science & Mathematics student at CU Boulder (BSc 2025) with a passion for eco-friendly software development, blockchain, network & embedded engineering, working with Artificial Intelligence, and teaching Calculus! I'm proficient in Typescript, PHP, C#, React(&Native), C++, Bash, and enjoy working with many other programming languages. I specialize in single-page application development, software code optimization, relational database management, and building embedded systems.
           
               </p>
 </b>
