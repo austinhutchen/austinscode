d4f8270c11956d2d105d6be0a1729c483f90fbb6
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-03-13 11:00:28 -0700

    tsconfig fixes

--- src/components/base/AnimatedRoutes.tsx
+++ src/components/base/AnimatedRoutes.tsx
@@ -1,31 +1,27 @@
-import { Landing } from "../pages/landing"
-import { AnimatePresence } from "framer-motion"; 
-import {Resume} from '../pages/resume'
-import {Projects} from '../pages/projects'
-import {Aboutme} from '../pages/aboutme'
-import {Feedback} from '../pages/feedback'
-import React from 'react'
-import { Routes, Route, useLocation } from "react-router-dom";
-import {Visualizer} from '../pages/gallery';
- 
+import React from 'react';
+import { AnimatePresence } from 'framer-motion';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { Landing } from '../pages/landing';
+import { Resume } from '../pages/resume';
+import { Projects } from '../pages/projects';
+import { Aboutme } from '../pages/aboutme';
+import { Feedback } from '../pages/feedback';
+import { Visualizer } from '../pages/gallery';
 
-export const AnimatedRoutes = () => {
+export const AnimatedRoutes: React.FC = () => {
     const location = useLocation();
     return (
-            <div className="App">
-                <AnimatePresence mode="wait">
-                    <Routes location={location} key={location.pathname}>
-                        <Route  path="" element={<Landing />} />
-                        <Route   path="/Resume" element={<Resume  />} />
-                        <Route   path="/Projects" element={<Projects />} />
-                        <Route  path="/AboutMe" element={<Aboutme />} />
-                        <Route   path="/Feedback" element={<Feedback />} />
-			<Route path ="/Gallery" element={<Visualizer /> } />
-                    </Routes>
-                </AnimatePresence>
-
-            </div>
-
-    )
-
-}
+        <div className="App">
+            <AnimatePresence exitBeforeEnter>
+                <Routes location={location}>
+                    <Route path="" element={<Landing />} />
+                    <Route path="/Resume" element={<Resume />} />
+                    <Route path="/Projects" element={<Projects />} />
+                    <Route path="/AboutMe" element={<Aboutme />} />
+                    <Route path="/Feedback" element={<Feedback />} />
+                    <Route path="/Gallery" element={<Visualizer />} />
+                </Routes>
+            </AnimatePresence>
+        </div>
+    );
+};
