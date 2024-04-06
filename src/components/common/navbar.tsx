2dfb2d60f2d277bdd0fdf4ea7bdcf07932f0905e
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-04-06 07:53:07 -0700

    custom hamburger toggler animated

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Collapse, Navbar, Nav, NavItem, NavbarBrand, NavbarToggler, Button } from 'reactstrap';
+import { Collapse, Navbar, Nav, NavItem, NavbarBrand, Button } from 'reactstrap';
 import "../../css/navBar.css";
 export const NavBar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isToggled, setIsToggled] = useState(false); // Add this line
   const nav = useNavigate();
   const github = process.env.PUBLIC_URL + '/fast_imgs/github.png';
   const instagram = process.env.PUBLIC_URL + '/fast_imgs/Instagram.png';
@@ -18,8 +19,24 @@ export const NavBar: React.FC = () => {
   const cube = process.env.PUBLIC_URL + '/fast_imgs/cube.png';
   const icon_size = '23vh';
 
-  const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    setIsToggled(true); // Set isToggled to true when the navbar is toggled
+  }
+  type CustomNavbarTogglerProps = {
+    onClick: () => void; // Define the onClick prop as a function that doesn't return anything
+    isOpen: boolean; // Add the isOpen prop
+    isToggled: boolean; // Add the isToggled prop
 
+  };
+
+  const CustomNavbarToggler: React.FC<CustomNavbarTogglerProps> = ({ onClick, isOpen }) => (
+    <button className="custom-navbar-toggler" onClick={onClick}>
+      <div id="line1" className={`line ${isOpen && isToggled ? 'line-open' : 'line-close'}`} />
+      <div id="line2" className={`line ${isOpen && isToggled ? 'line-open' : 'line-close'}`} />
+      <div id="line3" className={`line ${isOpen && isToggled ? 'line-open' : 'line-close'}`} />
+    </button>
+  );
   return (
     <div >
       <Navbar className="custom-navbar" >
@@ -27,8 +44,7 @@ export const NavBar: React.FC = () => {
 
           <NavbarBrand className='navBrand' id="name" >Austin Hutchen </NavbarBrand>
 
-          <NavbarToggler className="navbar-toggler" onClick={toggle} />
-        </div>
+          <CustomNavbarToggler onClick={toggle} isOpen={isOpen} isToggled={isToggled} />        </div>
 
         <Collapse isOpen={isOpen} navbar className="collapse-animaiton">
           <Nav className="mr-auto d-flex justify-content-center align-items-center" navbar>
@@ -40,42 +56,42 @@ export const NavBar: React.FC = () => {
             </NavItem >
             <NavItem className="nav-item">
               <Button style={{ border: '1px solid white' }} onClick={() => nav('/AboutMe')}>
-                <img src={about} width={icon_size} height={icon_size}  />
+                <img src={about} width={icon_size} height={icon_size} />
                 About
               </Button>
             </NavItem>
             <NavItem className="nav-item">
               <Button style={{ border: '1px solid white' }} onClick={() => nav('/Projects')}>
-                <img src={projects} width={icon_size} height={icon_size}  />
+                <img src={projects} width={icon_size} height={icon_size} />
                 Projects
               </Button>
             </NavItem>
             <NavItem className="nav-item">
               <Button style={{ border: '1px solid white' }} onClick={() => nav('/Resume')}>
-                <img src={resume} width={icon_size} height={icon_size}  />
+                <img src={resume} width={icon_size} height={icon_size} />
                 Resume
               </Button>
             </NavItem>
             <NavItem className="nav-item">
               <Button style={{ border: '1px solid white' }} onClick={() => nav('/Feedback')}>
-                <img src={feedback} width={icon_size} height={icon_size}  />
+                <img src={feedback} width={icon_size} height={icon_size} />
                 Feedback
               </Button>
             </NavItem>
-              <NavItem className="nav-item">
+            <NavItem className="nav-item">
               <Button style={{ border: '1px solid white' }} onClick={() => nav('/Gallery')}>
-                <img src={cube} width={icon_size} height={icon_size}  />
+                <img src={cube} width={icon_size} height={icon_size} />
                 Fun Page
               </Button>
             </NavItem>
             <NavItem className="nav-item" style={{ border: '1px solid white' }}>
-              <a  href="https://github.com/austinhutchen" target="_blank" rel="noopener noreferrer">
-                <img src={github} width={icon_size} height={icon_size}  />
+              <a href="https://github.com/austinhutchen" target="_blank" rel="noopener noreferrer">
+                <img src={github} width={icon_size} height={icon_size} />
                 Github
               </a>
             </NavItem>
             <NavItem style={{ border: '1px solid white' }} className="nav-item">
-              <a  href="https://www.instagram.com/austinscode/" target="_blank">
+              <a href="https://www.instagram.com/austinscode/" target="_blank">
                 <img src={instagram} width={icon_size} height={icon_size} className="nav-icon" />
                 Instagram
               </a>
@@ -93,7 +109,7 @@ export const NavBar: React.FC = () => {
               </a>
             </NavItem>
             <NavItem className="nav-item" style={{ border: '1px solid white' }}>
-              <a  href="https://austinhutchen.medium.com/" target="_blank">
+              <a href="https://austinhutchen.medium.com/" target="_blank">
                 <img src={medium} width={icon_size} height={icon_size} className="nav-icon" />
                 Blog
               </a>
