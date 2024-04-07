579a855a76eca73c42791cfb2616af65b8dbeb8b
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-04-07 07:15:17 -0700

    d

--- src/components/pages/gallery.tsx
+++ src/components/pages/gallery.tsx
@@ -1,39 +1,42 @@
-import React, { useRef, useEffect } from 'react';
+import React, { Suspense, useRef, useEffect, lazy } from 'react';
 import { NavBar } from '../common/navbar';
-import AudioVisualizer from "./sub/AudioVisualizer" ;
 import * as THREE from 'three';
 
-
+const AudioVisualizer = lazy(() => import('./sub/AudioVisualizer'));
+const TimeDomainVisualizer = lazy(() => import('./sub/AudioVisualizer'));
 export const Visualizer: React.FC = () => {
   return (
     <>
-    <NavBar />
+      <NavBar />
 
-    <div className='visualizer'>
+      <div className='visualizer'>
         <h1 className="hlight"> Welcome to my fun page!</h1>
-        <p className="lead"> <b> This page contains a gallery with live demos of more projects and tutorials, by me! </b> 
-        <br/>
+        <p className="lead"> <b> This page contains a gallery with live demos of more projects and tutorials, by me! </b>
+          <br />
 
         </p>
 
         <hr className="my-4" />
         <h2 className="hlight"> MICROPHONE SPECTRUM DECOMPOSITION</h2>
         <b>
-        <p style={{fontSize:"0.9em", fontFamily:"-moz-initial"}} >
-          <h3 class="hlight-mini">Enable microphone input </h3> to visualize this effect in real time with the spotify player interface below! This is an embedded web program that uses the fast fourier transform to decompose the audio spectrum of a microphone input. The program then displays the audio spectrum in an HTML canvas element.
-        </p>
+          <p style={{ fontSize: "0.9em", fontFamily: "-moz-initial" }} >
+            Enable microphone input to visualize this effect in real time with the spotify player interface below! This is an embedded web program that uses the fast fourier transform to decompose the audio spectrum of a microphone input. The program then displays the audio spectrum in an HTML canvas element.
+          </p>
         </b>
-<AudioVisualizer />
-    <br/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <AudioVisualizer />
+          <TimeDomainVisualizer />
+        </Suspense>
+        <br />
         <h2 className="hlight"> FAVORITE ALBUM PLAYER</h2>
         <b>
 
-        <p style={{fontSize:"0.9em", fontFamily:"-moz-initial"}} >
-          A spotify web player that displays my currently most played album and allows for interactive play. I embedded the code itself within an iframe to work on this website!
-        </p>
+          <p style={{ fontSize: "0.9em", fontFamily: "-moz-initial" }} >
+            A spotify web player that displays my currently most played album and allows for interactive play. I embedded the code itself within an iframe to work on this website!
+          </p>
         </b>
 
-        <iframe  src="https://open.spotify.com/embed/album/3RQQmkQEvNCY4prGKE6oc5?utm_source=generator" width="100%" height="480"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>        <br/>
+        <iframe src="https://open.spotify.com/embed/album/3RQQmkQEvNCY4prGKE6oc5?utm_source=generator" width="100%" height="480" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>        <br />
 
 
 
@@ -41,18 +44,17 @@ export const Visualizer: React.FC = () => {
           <h2 className="hlight"> 4-DIMENSIONAL TESSERACT / 3-D CUBE PROJECTION</h2>
 
         </b>
-        <p style={{fontSize:"0.9em", fontFamily:"-moz-initial"}} >
+        <p style={{ fontSize: "0.9em", fontFamily: "-moz-initial" }} >
           A tesseract is a four-dimensional analog to the cube. It is to a cube as a cube is to a square. Just as the surface of a cube consists of six square faces, the hypersurface of a tesseract consists of eight cubical cells. The tesseract is one of the six convex regular 4-polytopes. Here, I programmed a visualizer using three.js, a three-dimensional software that rotates a cube to project all of its faces.
         </p>
         <Tesseract />
-        
+
 
       </div>
 
     </>
   );
 };
-
 
 const Tesseract: React.FC = () => {
   const ref = useRef<HTMLDivElement>(null);
@@ -74,9 +76,10 @@ const Tesseract: React.FC = () => {
     scene.add(line);
 
     camera.position.z = 5;
+  let animationId: number;
 
     const animate = function () {
-      requestAnimationFrame(animate);
+    animationId = requestAnimationFrame(animate);
 
       line.rotation.x += 0.01;
       line.rotation.y += 0.01;
@@ -84,10 +87,23 @@ const Tesseract: React.FC = () => {
       renderer.render(scene, camera);
     };
 
+    const onWindowResize = () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener('resize', onWindowResize, false);
+
     animate();
+
+    return () => {
+      window.removeEventListener('resize', onWindowResize);
+    cancelAnimationFrame(animationId);
+      ref.current?.removeChild(renderer.domElement);
+    };
   }, []);
 
   return <div className="tesseract" ref={ref} />;
 };
-
 export default Tesseract;
