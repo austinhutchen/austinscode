ae8d407e67e62c282224bf57d70f9265725e80c9
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-04-30 10:34:35 -0600

    predeploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-{ pnpm run predeploy; pnpm run deploy; } 
+{ git add .; git commit -m 'predeploy'; git pull; git push; pnpm run predeploy; vercel --prod; pnpm run deploy; } || { echo "Deployment failed"; exit 1; }
