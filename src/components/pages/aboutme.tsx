664c1e73665e16302ce885fc0aac105a427aeb21
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2024-08-31 06:16:33 -0600

    refactor image loading on aboutme

--- src/components/pages/aboutme.tsx
+++ src/components/pages/aboutme.tsx
@@ -3,75 +3,81 @@ import { NavBar } from "../common/navbar";
 import Slider from "react-slick";
 import { HiArrowNarrowRight, HiArrowNarrowLeft } from 'react-icons/hi';
 import "../../css/slider.css";
+const getImgPath = (imageName: string) => `${process.env.PUBLIC_URL}/fast_imgs/${imageName}`;
+type Images = Record<string, string>;
 
-const tinker = process.env.PUBLIC_URL + '/fast_imgs/tinker.webp';
-const me = process.env.PUBLIC_URL + '/fast_imgs/austinselfie.jpg';
-const coding = process.env.PUBLIC_URL + '/fast_imgs/coding.webp';
-const systems = process.env.PUBLIC_URL + '/fast_imgs/systems.webp';
-const math = process.env.PUBLIC_URL + '/fast_imgs/IMG_0913.webp';
-const family = process.env.PUBLIC_URL + '/fast_imgs/me.webp';
-const mimi = process.env.PUBLIC_URL + '/fast_imgs/mimi.webp';
-const liv = process.env.PUBLIC_URL + '/fast_imgs/liv.webp';
-const solder = process.env.PUBLIC_URL + '/fast_imgs/SOLDER.mp4';
-const nature = process.env.PUBLIC_URL + '/fast_imgs/nature.webp';
-const github = process.env.PUBLIC_URL + '/fast_imgs/GITHUBME.webp';
+const images: Images = {
+  tinker: getImgPath("tinker.webp"),
+  me: getImgPath("austinselfie.jpg"),
+  coding: getImgPath("coding.webp"),
+  systems: getImgPath("sytems.webp"),
+  math: getImgPath("IMG_0913.webp"),
+  family : getImgPath("me.webp"),
+  mimi : getImgPath("mimi.webp"),
+  solder: getImgPath("SOLDER.mp4"),
+  liv: getImgPath("liv.webp"),
+  nature: getImgPath("nature.webp"),
+  github: getImgPath("GITHUBME.webp")
+  // Add other images here
+};
+
 
 // Define image and video URLs directly in the keys array
-const keys = [
+ const keys = [
   {
     desc: "Welcome! I'm Austin, a Computer and Software engineer who likes to tinker with electronic circuits and code Typescript-based software applications, and I have plenty of hobbies! ",
-    url: me,
+    url: images.me,
     type: "image"
   },
   {
     desc: "Ever since I was little, I have loved to experiment with electronics. Here is a 3-volt windmill I built out of wood and craft materials, at the age of 11.",
-    url: tinker,
+    url: images.tinker,
     type: "image"
   },
   {
     desc: "Today in my free time, I love to optimize and build software! I practice leetcode frequently to refine my understanding of algorithms and enjoy comparing their runtime complexities. Check out my solutions! ",
-    url: coding,
+    url: images.coding,
     type: "image"
   },
   {
     desc: "I enjoy building websites like this one, apps like the ones on my projects page, and working with server scripts on the backend. ",
-    url: github,
+    url: images.github,
     type: "image"
   },
   {
     desc: "I also spend time building embedded systems to automate daily tasks. I use recycled e-waste for some project materials to save the planet, and make scripts using PHP and C.",
-    url: systems,
+    url: images.systems,
     type: "image"
   },
   {
     desc: "I love to solder! Soldering has allowed me to create my own PCBS, and build embedded systems with common and recycled parts.",
-    url: solder,
+    url: images.solder,
     type: "video"
   },
   {
     desc: "I love spending time with my family.",
-    url: family,
+    url: images.family,
     type: "image"
   },
   {
     desc: "I love spending time with my cats! Cats are the most calming pets to be around, in my opinion. I could code around them forever. ",
-    url: mimi,
+    url: images.mimi,
     type: "image"
   },
   {
     desc: "I read mathematics from my personally collected library in my free time. I have particular fascinations with Calculus, Boolean & Linear Algebra, and Group Theory.",
-    url: math,
+    url: images.math,
     type: "image"
   },
 
   {
     desc: "I love to make electronics and art with my girlfriend!",
-    url: liv,
+    url: images.liv,
     type: "image"
   },
   {
     desc: "I love to spend time out in nature!",
-    url: nature,
+    url: images.nature,
     type: "image"
   },
   // ... other items ...
