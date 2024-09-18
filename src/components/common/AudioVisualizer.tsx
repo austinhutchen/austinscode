33b6876a71ae447e979963bdd82dd6df52965d4a
Austin Hutchen <hutchenaustin@gmail.com>
2024-09-18 08:02:03 -0600

    optimizations and file size halved

--- src/components/common/AudioVisualizer.tsx
+++ src/components/common/AudioVisualizer.tsx
@@ -76,7 +76,7 @@ export const AudioVisualizer: React.FC<AudioVisualizerProps> = () => {
   }, [stream]);
   return <><button style={{}} onClick={getUserMedia}>Click & Speak! </button>
     <br />
-    <canvas ref={canvasRef} style={{ width: '45vw', height: '35vh', borderRadius: '3px', border: '1px solid #0FF' }} />
+    <canvas ref={canvasRef} style={{ width: '55svw', height: '35vh', borderRadius: '3px', border: '1px solid #0FF' }} />
 
   </>
 
