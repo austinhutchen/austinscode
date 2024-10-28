cd5aec0fd84f95afb0447a80a7e101bea0a0e1f9
Austin Hutchen <hutchencodes@gmail.com>
2024-10-27 18:19:53 -0600

    fix

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -43,7 +43,7 @@ export const NavBar: React.FC = () => {
     <Navbar className="custom-navbar">
       <div className="navbar-header flex ">
         <NavbarBrand className="navBrand" id="name">Austin Hutchen</NavbarBrand>
-        <img className="clickIcon" width="60vw"  height="60vh" src={clickme} />
+        <img className="clickIcon" width="65vw"  height="65vh" src={clickme} />
         <CustomNavbarToggler onClick={toggle} isOpen={isOpen} isToggled={isToggled} />
       </div>
       <Collapse isOpen={isOpen} navbar className="navFlex">
