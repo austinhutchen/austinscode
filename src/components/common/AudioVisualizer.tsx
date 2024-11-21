458703d68417427d0b88d0e066fc5d9d27c4d6b8
Austin Hutchen <austinhutchen@Austins-MacBook-Air.local>
2024-11-21 11:45:06 -0700

    navbar

--- src/components/common/AudioVisualizer.tsx
+++ src/components/common/AudioVisualizer.tsx
@@ -78,7 +78,7 @@ export const AudioVisualizer: React.FC<AudioVisualizerProps> = () => {
   }, [stream]);
   return <><button style={{}} onClick={getUserMedia}>Click & Speak! </button>
     <br />
-    <canvas ref={canvasRef} style={{ width: '65svw', height: '28svh', border: '0.5px solid #0FF' }} />
+    <canvas ref={canvasRef} style={{ width: '65svw', height: '28svh', border: '0.5px solid #0FF', borderRadius:'3svw' }} />
 
   </>
 
