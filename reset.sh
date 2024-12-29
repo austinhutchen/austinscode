c8b166e3dcb94325fe49b250ff5aa240b02da1a6
Austin Hutchen <austinhutchen@MacBookAir.attlocal.net>
2024-12-29 01:19:39 -0800

    projects drop shadow

--- reset.sh
+++ reset.sh
@@ -3,8 +3,8 @@
   npm cache clean --force
   npm cache clear --force
  npm cache verify
-  rm -rf package-lock.json
+npm install 
+ rm -rf package-lock.json
   rm -rf node_modules
-npm install --registry=https://registry.npmjs.org/  
   npm run deploy
-} &
+}&
