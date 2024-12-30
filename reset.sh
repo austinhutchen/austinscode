329d4bfa77ee8ef5642552d1408d36305fe077d2
Austin Hutchen <austinhutchen@Austins-MacBook-Air.local>
2024-12-30 07:31:48 -0800

    pics

--- reset.sh
+++ reset.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 {
-  npm cache clean --force
-  npm cache clear --force
- npm cache verify
-npm install 
- rm -rf package-lock.json
-  rm -rf node_modules
+npm install
   npm run deploy
-}&
+}
