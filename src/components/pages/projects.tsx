4ea3a0689dbbc414925e9ff8d04e5b9e9dba0c30
Austin Hutchen <austinhutchen@MacBookAir.attlocal.net>
2025-01-07 10:50:44 -0800

    pkg

--- src/components/pages/projects.tsx
+++ src/components/pages/projects.tsx
@@ -192,7 +192,6 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ project }) => {
               } else if (mediaRef instanceof HTMLVideoElement) {
                 const source = document.createElement('source');
                 source.src = mediaRef.dataset.src || '';
-                mediaRef.loading="lazy";
                 mediaRef.appendChild(source);
                 mediaRef.load();
               }
