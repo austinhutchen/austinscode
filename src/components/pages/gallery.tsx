bbfb21d600ad4538337132be05daf25fa05213e9
Austin Hutchen <austinhutchen@MacBookAir.attlocal.net>
2025-01-07 18:15:13 -0800

    fix

--- src/components/pages/gallery.tsx
+++ src/components/pages/gallery.tsx
@@ -19,8 +19,9 @@ export const Visualizer: React.FC = () => {
       <NavBar />
 
       <h1 className="hlight"> Welcome to my fun page!</h1>
-      <p className="lead"> <b> This page contains a gallery with live demos of more projects, analog circuits, and short tutorials by me! </b> </p>
+      <h3 className="hlight"> <b> This page contains a gallery with live demos of more projects, analog circuits, and short tutorials by me! </b> </h3>
      <a href="https://github.com/austinhutchen/circuits" rel="nopener noreferrer"><h3>Full repository with all circuit schematics in PDF format</h3></a>
+      <br/>
       <b>
         <h2 className="hlight"> BIPOLAR LINEAR POWER SUPPLY </h2>
       </b>
