8e3cf2ca822ee88f025763199489cc3b69ab1c6d
Austin Hutchen <austinhutchen@Austins-MacBook-Air.local>
2025-01-29 19:08:37 -0700

    update cache

--- generate-cache.js
+++ generate-cache.js
@@ -2,12 +2,22 @@ const fs = require("fs");
 const path = require("path");
 
 const imageDir = path.join(__dirname, "public/fast_imgs");
+const buildCssDir = path.join(__dirname, "build/static/css"); // React build CSS path
 const outputFile = path.join(__dirname, "public/assets-to-cache.json");
 
-// Get all images in the directory
-const images = fs.readdirSync(imageDir).map((file) => `/fast_imgs/${file}`);
+function getFilesFromDir(directory, basePath) {
+  return fs.existsSync(directory)
+    ? fs.readdirSync(directory).map((file) => `/${basePath}/${file}`)
+    : [];
+}
+
+// Get images and CSS from the React build
+const images = getFilesFromDir(imageDir, "fast_imgs");
+const cssFiles = getFilesFromDir(buildCssDir, "static/css");
+
+const assetsToCache = [...images, ...cssFiles];
 
 // Write the asset list to a JSON file
-fs.writeFileSync(outputFile, JSON.stringify(images, null, 2));
+fs.writeFileSync(outputFile, JSON.stringify(assetsToCache, null, 2));
 
-console.log("Assets to Cache:", images);
+console.log("Assets to Cache:", assetsToCache);
