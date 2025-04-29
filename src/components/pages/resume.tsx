56bfc3b4dd8d1376ac4bc804fff929852ce0ae14
Austin Hutchen <austinhutchen@Mac.lan>
2025-04-29 07:50:38 -0700

    forest gradient test

--- src/components/pages/resume.tsx
+++ src/components/pages/resume.tsx
@@ -161,7 +161,7 @@ const experienceItems = [
         period: "May 2024 - August 2024",
       description: (
   <>
-  Built and tested an RS485 C# NuGet library for <a href="https://www.modbus.org/docs/Modbus_Messaging_Implementation_Guide_V1_0b.pdf" rel="noopener noreferrer">ModBus ASCII/RTU </a> drivers and simulators used at{" "}
+  Built and tested an RS485 C# library (NuGet) for <a href="https://www.modbus.org/docs/Modbus_Messaging_Implementation_Guide_V1_0b.pdf" rel="noopener noreferrer">ModBus ASCII/RTU </a> drivers & simulators used at{" "}
 <a href="https://www.formfactor.com/" rel="noopener noreferrer">FormFactor</a>. Contributed to development on FormFactor Cryo's IQ-3000 Spin-Qubit quantum computer system, supporting near-zero Kelvin operations. In meeting with Cryo team, drafted engineering decisions for system optimization. </>
 ),    },
     {
